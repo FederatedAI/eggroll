4341c6d5718eae8213ed0c651d5b0b970db39cde
itboyljm <itboyljm@gmail.com>
2019-08-15 20:48:31 +0800

    Fixes ISSUE#19
    Changes:
    
    1.based on processor adapter，support rocksdb and network.

--- computing/storage_adapters.py
+++ computing/storage_adapters.py
@@ -1,10 +1,17 @@
+# Copyright (c) 2019 - now, Eggroll Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-#WARNING: all apis in this module are internal developing now.
-
-LMDB_MAP_SIZE = 16 * 4_096 * 244_140        # follows storage-service-cxx's config here
-DEFAULT_DB = b'main'
-DELIMETER = '-'
-DELIMETER_ENCODED = DELIMETER.encode()
 import os
 import grpc
 from eggroll.api.proto import kv_pb2, kv_pb2_grpc
@@ -23,6 +30,10 @@ try:
 except:
     LOGGER.info("WRAN: failed to import lmdb")
 
+LMDB_MAP_SIZE = 16 * 4_096 * 244_140        # follows storage-service-cxx's config here
+DEFAULT_DB = b'main'
+DELIMETER = '-'
+DELIMETER_ENCODED = DELIMETER.encode()
 
 class AdapterManager:
     pass
