5419c4b2d9f080f9a24fbe31cce944dc118e113c
maxwong <maxwong@live.cn>
2019-09-04 15:52:14 +0800

    1. Fixes configuration bugs in codes. 2. Removes confilct black lists.

--- core/src/main/java/com/webank/ai/eggroll/core/utils/PropertyGetter.java
+++ core/src/main/java/com/webank/ai/eggroll/core/utils/PropertyGetter.java
@@ -1,13 +1,8 @@
 package com.webank.ai.eggroll.core.utils;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.Properties;
 
-@Component
-@Scope("prototype")
 public interface PropertyGetter {
     public boolean addSource(Properties prop);
     public List<Properties> getAllSources();
