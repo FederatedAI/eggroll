c84e794cd391459b149dfd74c8a452415980095b
huangqijun <huangqijun@baidu.com>
2019-12-01 12:10:01 +0800

    add: eggroll_boot

---
+++ bin/eggroll_boot.sh
@@ -0,0 +1,14 @@
+sub_cmd=$1
+exe=$2
+session_id=$3
+node_id=$4
+SHELL_FOLDER=$(dirname "$0")
+pid_file=$SHELL_FOLDER/pid/$session_id-$node_id.pid
+
+if [ $sub_cmd == "start_node" ]
+then
+  exec $exe &
+  echo $! > $SHELL_FOLDER/pid/$session_id-$node_id.pid
+else
+  kill `cat $pid_file`
+fi
