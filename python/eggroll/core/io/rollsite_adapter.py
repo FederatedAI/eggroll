bc04b58bb4782220319c4ceaecf8aab090351778
wenzanhuang <154000436@qq.com>
2019-12-02 15:33:29 +0800

    add putBatch

--- python/eggroll/core/io/rollsite_adapter.py
+++ python/eggroll/core/io/rollsite_adapter.py
@@ -17,39 +17,79 @@ import sys
 import os
 import pickle
 
-#sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
-from eggroll.roll_site.api import rollsite
+from eggroll.roll_site import rollsite
 from eggroll.core.io.kv_adapter import SortedKvWriteBatch
 from eggroll.core.io.kv_adapter import SortedKvIterator
 from eggroll.core.io.kv_adapter import SortedKvAdapter
+
 from eggroll.core.proto import meta_pb2
 from eggroll.core.utils import _elements_to_proto
 
-class RollsiteWriteBatch(SortedKvWriteBatch):
+from eggroll.core.io.format import BinBatchWriter
 
+class RollsiteWriteBatch(SortedKvWriteBatch):
   def __init__(self, adapter):
     self.adapter = adapter
     #self.kv_stub = self.adapter.kv_stub
     self.cache = []
     self.name = adapter._name
     self.tag = adapter._tag
+    self.__bin_packet_len = 1 << 20
+    self.buffer = bytearray(self.__bin_packet_len)
+    self.writer = BinBatchWriter({'buffer': self.buffer})
+    self.cur_offset = self.writer.get_offset
+    self.total_written = 0
+    self.dst_host = adapter._dst_host
+    self.dst_port = adapter._dst_port
 
-  def write(self):
-    if self.cache:
-      #self.kv_stub.putAll(iter(self.cache), metadata=self.adapter.get_stream_meta())
-      #rollsite.push(content, "model_A", tag="{}".format(_tag))
-      print(self.cache)
-      rollsite.push(str(pickle.dumps(self.cache)), self.name, self.tag)
-      self.cache.clear()
+  def transfer_batch_generator(self, packet_len, k_bytes, v_bytes):
+    # todo: pull up as format
+    buffer = bytearray(packet_len)
+    writer = BinBatchWriter({'buffer': buffer})
+    self.cur_offset = writer.get_offset()
+    total_written = 0
+    try:
+      writer.write_bytes(k_bytes, include_size=True)
+      writer.write_bytes(v_bytes, include_size=True)
+      total_written += 1
+      if tag == '1-0':
+        print(f'{tag} written {k_bytes}, total_written {total_written}, is closable {broker.is_closable()}')
+    except IndexError as e:
+      print('caught index error')
+
+  def write(self, bin_data):
+    print(bin_data)
+    rollsite.push(bin_data, self.name, self.tag)
+    self.cache.clear()
 
   def close(self):
     # write last
-    self.write()
+    writer = self.writer
+    self.cur_offset = writer.get_offset()
+    bin_data = writer.get_batch(end=self.cur_offset)
+    self.write(bin_data)
 
   def put(self, k, v):
-    self.cache.append((k, v))
-    if len(self.cache) > 100000:
-      self.write()
+    writer = self.writer
+    try:
+      writer.write_bytes(k, include_size=True)
+      writer.write_bytes(v, include_size=True)
+      self.total_written += 1
+      if self.tag == '1-0':
+        print(f'{self.tag} written {k_bytes}, total_written {self.total_written}, is closable {broker.is_closable()}')
+    except IndexError as e:
+      print('caught index error')
+      bin_data = writer.get_batch(end=self.cur_offset)
+
+      self.write(bin_data)
+
+      self.writer = BinBatchWriter({'buffer': self.buffer})
+      writer = self.writer
+      writer.write_bytes(k, include_size=True)
+      writer.write_bytes(v, include_size=True)
+    except:
+      print("Unexpected error:", sys.exc_info()[0])
+      raise
 
 
 class RollsiteIterator(SortedKvIterator):
@@ -85,14 +125,6 @@ class RollsiteIterator(SortedKvIterator):
     return self.it
 
 class RollsiteAdapter(SortedKvAdapter):
-  # options:
-  #   host
-  #   port
-  #   store_type
-  #   name
-  #   namespace
-  #   fragment
-
   def get_stream_meta(self):
     return ('store_type', self.options["store_type"]), \
            ('table_name', self.options["name"]), \
@@ -103,27 +135,24 @@ class RollsiteAdapter(SortedKvAdapter):
     super().__init__(options)
     self.options = options
 
-    rollsite.init("atest",
-                  "roll_site/test/role_conf.json",
-                  "roll_site/test/server_conf.json",
-                  "roll_site/test/transfer_conf.json")
-
     self._namespace = ''
     self._name = options["name"]
-    self._tag = options["tag"]
+    self._tag = 'tag'
     self._store_type = 'roll_site'
     self._path = ''
     self._partitioner = ''
     self._serdes = ''
     self._partitions = []
+    self._dst_host = 'localhost'
+    self._dst_port = '9394'
     self._store_locator = meta_pb2.StoreLocator(storeType=self._store_type,
                                                 namespace=self._namespace,
                                                 name=self._name,
                                                 path=self._path,
                                                 partitioner=self._partitioner,
                                                 serdes=self._serdes)
+
     '''
-    #这个是egg 中数据库的host和port
     host = options["host"]
     port = options["port"]
     self.channel = grpc.insecure_channel(target="{}:{}".format(host, port),
@@ -131,6 +160,7 @@ class RollsiteAdapter(SortedKvAdapter):
                                                   ('grpc.max_receive_message_length', -1)])
     self.kv_stub = kv_pb2_grpc.KVServiceStub(self.channel)
     '''
+
   def to_proto(self):
     return meta_pb2.Store(storeLocator=self._store_locator,
                           partitions=_elements_to_proto(self._partitions))
