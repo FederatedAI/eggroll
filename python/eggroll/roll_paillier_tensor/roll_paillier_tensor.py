28b3106e7be91051a00db82dc774ed4364358365
chanzhennan <chanzhennan@163.com>
2019-12-13 11:27:48 +0800

    testLR v1.1

--- python/eggroll/roll_paillier_tensor/roll_paillier_tensor.py
+++ python/eggroll/roll_paillier_tensor/roll_paillier_tensor.py
@@ -18,10 +18,15 @@ from eggroll.core.meta_model import ErStoreLocator, ErJob, ErStore, ErFunctor
 from eggroll.core.proto import command_pb2_grpc
 from eggroll.core.serdes import cloudpickle
 from eggroll.roll_pair.roll_pair import RollPair
+from eggroll.roll_pair.roll_pair import RollPairContext
+from eggroll.utils import log_utils
+import numpy as np
+
 import roll_paillier_tensor as rpt_engine
 
-from eggroll.roll_pair.roll_pair import RollPairContext
 
+log_utils.setDirectory()
+LOGGER = log_utils.getLogger()
 class RptBaseEngine:
     def load(self, x):
         raise NotImplementedError("todo")
@@ -36,33 +41,18 @@ class RptGpuEngine(RptBaseEngine):
 
 class RptCpuEngine(RptBaseEngine):
     def __init__(self, pub_key=None, priv_key=None):
-        print("+++++++++++++++++++++3")
-        self.pub_key = rpt_engine.load_pub_key(pub_key)
-        self.priv_key = rpt_engine.load_prv_key(priv_key)
+        #print("[ RptCpuEngine __init__ ]", pub_key, priv_key)
+        self.pub_key = pub_key
+        self.priv_key = priv_key
 
-    # def __setstate__(self, state):
-    #     bin_pub,bin_prv = state
-    #     self.priv_key=rpt_engine.load_prv_key(bin_pub)
-    #     self.pub_key=rpt_engine.load_pub_key(bin_prv)
-    #
-    # def __getstate__(self):
-    #     rpt_engine.dump_pub_key(self.pub_key)
-    #     rpt_engine.dump_prv_key(self.priv_key)
+    def __setstate__(self, state):
+        bin_pub, bin_prv = state
+        print("[__setstate__]++++++++++++++++++++++")
+        self.pub_key = rpt_engine.load_pub_key(bin_pub)
+        self.priv_key = rpt_engine.load_prv_key(bin_prv)
 
-    def __transient__(self):
-        return []
-
-    def load(self,x):
-        return rpt_engine.load(x)
-
-    def dump(self, x):
-        return rpt_engine.dump(x)
-
-    def scalar_mul(self, x, scale):
-        return rpt_engine.add(x, scale, self.pub_key, self.priv_key)
-
-    def obf(self, x):
-        return rpt_engine.obf(x, self.pub_key, self.priv_key)
+    def __getstate__(self):
+        return rpt_engine.dump_pub_key(self.pub_key), rpt_engine.dump_prv_key(self.priv_key)
 
     def add(self, x, y):
         return rpt_engine.add(x, y, self.pub_key, self.priv_key)
@@ -70,177 +60,250 @@ class RptCpuEngine(RptBaseEngine):
     def vdot(self, x, y):
         return rpt_engine.vdot(x, y, self.pub_key, self.priv_key)
 
+    def scalar_mul(self, x, scale):
+        return rpt_engine.add(x, scale, self.pub_key, self.priv_key)
+
+    def matmul(self, x, y):
+        return rpt_engine.matmul(x, y, self.pub_key, self.priv_key)
+
+    def matmul_c_eql(self, x, y):
+        return rpt_engine.matmul_c_eql(x, y, self.pub_key, self.priv_key)
+
+    def matmul_r_eql(self, x, y):
+        return rpt_engine.matmul_r_eql(x, y, self.pub_key, self.priv_key)
+
+    # tool
+    def load(self, x):
+        return rpt_engine.load(x)
+
+    def dump(self, x):
+        return rpt_engine.dump(x)
+
+    def obf(self, x):
+        return rpt_engine.obf(x, self.pub_key, self.priv_key)
+
     def encrypt(self, x):
         return rpt_engine.encrypt(x, self.pub_key, self.priv_key)
 
     def decrypt(self, x):
         return rpt_engine.decrypt(x, self.pub_key, self.priv_key)
 
+    def decode(self, x):
+        return rpt_engine.decode(x, self.pub_key, self.priv_key)
+
+    #interface
+    def manager(self, x, y, val):
+        return rpt_engine.make_manager(x, y, val, self.pub_key)
+
+class RptLocalTensor(object):
+    def __init__(self, row, col, value, pub_key=None, priv_key=None):
+        self.pub_key = pub_key
+        self.priv_key = priv_key
+        self.data = self.localTensor(row, col, value)
+
+    def __setstate__(self, state):
+        data = state
+        self.data = rpt_engine.load(data)
+    def __getstate__(self):
+        return rpt_engine.dump(self.data)
+
+    def localTensor(self, row, col, val):
+        return rpt_engine.make_manager(row, col, val, self.pub_key)
+
+    def decode(self):
+        return rpt_engine.decode(self.data, self.pub_key,self.priv_key)
+
+
 class RptMixedEngine(RptBaseEngine):
     pass
 
 class RptContext:
-  def __init__(self, rp_ctx:RollPairContext):
-    self.rp_ctx = rp_ctx
+    def __init__(self, rp_ctx:RollPairContext):
+        self.rp_ctx = rp_ctx
 
-  def load(self, namespace, name, engine_type="cpu"):
-    return RollPaillierTensor(self.rp_ctx.load(name, namespace), engine_type)
+    def load(self, namespace, name, engine_type="cpu"):
+        return RollPaillierTensor(self.rp_ctx.load(namespace, name), engine_type)
 
 class RollPaillierTensor(object):
-  def __init__(self, roll_pair, engine_type="cpu"):
-    self._store = roll_pair
-    if engine_type == "cpu":
-        self.pub_key, self.prv_key = rpt_engine.keygen()
-        # pub_key, prv_key = rpt_engine.keygen()
-        self.pub_key = rpt_engine.dump_pub_key(self.pub_key)
-        self.prv_key = rpt_engine.dump_prv_key(self.prv_key)
-        # self._engine = RptCpuEngine(rpt_engine.dump_pub_key(self.pub_key),
-        #                             rpt_engine.dump_prv_key(self.prv_key))
-        print("+++++++++++++++++++++1")
-    else:
-        raise NotImplementedError("todo")
+    def __init__(self, roll_pair, engine_type="cpu"):
+        self._store = roll_pair
+        if engine_type == "cpu":
+            self.pub_key, self.prv_key = rpt_engine.keygen()
+            # pub_key, prv_key = rpt_engine.keygen()
+            # self.pub_key = rpt_engine.dump_pub_key(self.pub_key)
+            # self.prv_key = rpt_engine.dump_prv_key(self.prv_key)
+            self._engine = RptCpuEngine(self.pub_key, self.prv_key)
+        else:
+            raise NotImplementedError("todo")
 
-  def put_all(self, data):
-      self._store.put_all(data)
+    def put_all(self, data, options={}):
+        self._store.put_all(data, options=options)
 
-  def get_all(self):
-      data_list = self._store.get_all()
-      print(data_list)
-      return b'100'
+    def get_all(self):
+        data_list = self._store.get_all()
+        #print(data_list)
+        return b'100'
 
-  def scalar_mul(self, scalar):
-    def functor(mat, scalar):
-      mat_enc = self._engine.load(mat)
-      mat_res_enc = self._engine.scalar_mul(mat_enc, scalar)
+    def scalar_mul(self, scalar):
+        def functor(mat, scalar):
+            mat_enc = self._engine.load(mat)
+            mat_res_enc = self._engine.scalar_mul(mat_enc, scalar)
 
-      return rpt_engine.dump(mat_res_enc)
+            return rpt_engine.dump(mat_res_enc)
 
-    return self._store.map_values(lambda v: functor(v, float(scalar)))
+        return self._store.map_values(lambda v: functor(v, float(scalar)))
 
-  def obf(self):
-      def functor(mat):
-          print("[cpu_add]")
-          mat_enc = self._engine.load(mat)
-          mat_enc_obf = self._engine.obfuscate(mat)
-          return self._engine.dump(mat_enc_obf)
+    def obf(self):
+        def functor(mat):
+            # print("[cpu_add]")
+            mat_enc = self._engine.load(mat)
+            mat_enc_obf = self._engine.obfuscate(mat)
+            return self._engine.dump(mat_enc_obf)
 
-      return RollPaillierTensor(self._store.map_values(lambda mat: functor(mat)))
+        return RollPaillierTensor(self._store.map_values(lambda mat: functor(mat)))
 
-  def add(self, other):
-    pub_key = self.pub_key
-    prv_key = self.prv_key
+    def add(self, other):
+        # pub_key = self.pub_key
+        # prv_key = self.prv_key
+        _engine = self._engine
+        def functor(mat1, mat2):
+            mat1_enc = _engine.load(mat1)
+            mat2_enc = _engine.load(mat2)
 
-    print(prv_key)
-    def functor(mat1, mat2):
-      _engine = RptCpuEngine(pub_key, prv_key)
+            mat_res_enc = _engine.add(mat1_enc, mat2_enc)
+            return _engine.dump(mat_res_enc)
 
-      print("[cpu_add]")
+        return RollPaillierTensor(self._store.join(other._store, lambda mat1, mat2: functor(mat1, mat2)))
 
-      print("+++++++++++++++++++++")
+    def vdot(self, other):
 
-      mat1_enc = _engine.load(mat1)
-      mat2_enc = _engine.load(mat2)
+        _engine = self._engine
+        def functor(mat1, mat2):
+            mat1_enc = _engine.load(mat1)
+            mat2_enc = _engine.load(mat2)
+            mat2_pln = _engine.decrypt(mat1_enc)
 
-      mat_res_enc = _engine.add(mat1_enc, mat2_enc)
-      return _engine.dump(mat_res_enc)
+            mat_res_enc = _engine.vdot(mat1_enc, mat2_pln)
+            return rpt_engine.dump(mat_res_enc)
 
-    return RollPaillierTensor(self._store.join(other._store, lambda mat1, mat2: functor(mat1, mat2)))
+        return RollPaillierTensor(self._store.join(other._store, lambda left, right : functor(left, right)))
 
-  def vdot(self, other):
-      def functor(mat1, mat2):
-          print("[cpu dotmul]")
-          mat1_enc = self._engine.load(mat1)
-          mat2_enc = self._engine.load(mat2)
-          mat2_pln = self._engine.decrypt(mat1_enc)
+    def matmul(self, other):
+        _engine = self._engine
+        def seq_op(mat1, mat2):
+            print("python mat_mul here")
+            mat1_enc = _engine.load(mat1)
+            mat2_enc = _engine.load(mat2)
+            mat2_pln = _engine.decrypt(mat2_enc)
 
-          mat_res_enc = self._engine.vdot(mat1_enc, mat2_pln)
-          return rpt_engine.dump(mat_res_enc)
+            sub_res = _engine.matmul(mat1_enc, mat2_pln)
+            dumping = _engine.dump(sub_res)
+            return dumping
 
-      return RollPaillierTensor(self._store.join(other._store, lambda left, right : functor(left, right)))
+        return RollPaillierTensor(self._store.join(other._store, lambda mat1, mat2 : seq_op(mat1, mat2)))
 
-  def mat_mul(self, other):
-      def seq_op(left, right):
-          print("cpu mat_mul here")
-          pub_key, pvt_key = rpt_engine.keygen()
-          enc_mat1 = rpt_engine.load(left)
-          enc_mat2 = rpt_engine.load(right)
-          pln_mat2 = rpt_engine.decrypt(enc_mat2, pub_key, pvt_key)
+    def matmul_r_eql(self, other):
+        _engine = self._engine
+        def seq_op(mat1, mat2):
 
-          sub_res = rpt_engine.matmul_c_eql(enc_mat1, pln_mat2, pub_key, pvt_key)
-          dumping = rpt_engine.dump(sub_res)
-          return dumping
+            LOGGER.info("python mat_mul_r_eql here")
+            mat1_enc = _engine.load(mat1)
+            mat2_pln = _engine.load(mat2)
+            # #mat2_pln = _engine.decrypt(mat2_enc)
+            #
+            sub_res = _engine.matmul_r_eql(mat1_enc, mat2_pln)
+            # dumping = _engine.dump(sub_res)
+            return b'asdfafs'
 
-      def comb_op(left, right):
-          print(left + right)
+        tmp = self._store.join(other._store, lambda mat1, mat2: seq_op(mat1, mat2))
 
-      partition_result = self._store.join(other._store, lambda left, right : seq_op(left, right))
-      # print(partition_result)
-      #
-      rppr = partition_result
-      final_result = rppr.reduce(comb_op)
-      return final_result
+        return RollPaillierTensor(tmp)
 
-  def encrypt(self):
-      def functor(mat):
-          print("[cpu_add]")
-          mat_pln = self._engine.load(mat)
-          mat_enc = self._engine.encrypt_and_obfuscate(mat)
-          return self._engine.dump(mat_enc)
+    def matmul_c_eql(self, other):
+        _engine = self._engine
+        def seq_op(mat1, mat2):
+            print("python mat_mul_r_eql here")
+            mat1_enc = _engine.load(mat1)
+            mat2_enc = _engine.load(mat2)
+            mat2_pln = _engine.decrypt(mat2_enc)
 
-      return RollPaillierTensor(self._store.map_values(lambda mat: functor(mat)))
+            sub_res = _engine.matmul_c_eql(mat1_enc, mat2_pln)
+            dumping = _engine.dump(sub_res)
+            return dumping
+        print("step 1")
+        tmp = self._store.join(other._store, lambda mat1, mat2: seq_op(mat1, mat2))
+        print("step 2")
+        return RollPaillierTensor(tmp)
 
-  def decrypt(self):
-      def functor(mat):
-          print("[cpu_add]")
-          mat_enc = self._engine.load(mat)
-          mat_pln = self._engine.decrypt(mat_enc)
-          return self._engine.dump(mat_pln)
+    def encrypt(self):
+        _engine = self._engine
+        def functor(mat):
+            print("[cpu_encrypt]")
+            mat_pln = _engine.load(mat)
+            mat_enc = _engine.encrypt_and_obfuscate(mat)
+            return _engine.dump(mat_enc)
 
-      return RollPaillierTensor(self._store.map_values(lambda mat: functor(mat)))
+        return RollPaillierTensor(self._store.map_values(lambda mat: functor(mat)))
+
+    def decrypt(self):
+        _engine = self._engine
+        def functor(mat):
+           # _engine = RptCpuEngine(pub_key, prv_key)
+            mat_enc = _engine.load(mat)
+            mat_pln = _engine.decrypt(mat_enc)
+            return _engine.dump(mat_pln)
+        return RollPaillierTensor(self._store.map_values(lambda mat: functor(mat)))
+
+    def decode(self):
+        _engine=self._engine
+        def functor(mat):
+            #_engine = RptCpuEngine(pub_key, prv_key)
+            mat_code = _engine.load(mat)
+            print('[RptCpuEngine] ', _engine)
+            print('dir in rpt cpu engine', dir(_engine))
+            mat_org = _engine.decode(mat_code)
+            return mat_org
+        return RollPaillierTensor(self._store.map_values(lambda mat: functor(mat)))
 
 
-  # def gpu_add(self, other):
-  #   def functor(left, right):
-  #     pub_key, pvt_key = rpt_engine.gpu_genkey()
-  #
-  #     enc_mat1 = rpt_engine.gpu_load(left)
-  #     enc_mat2 = rpt_engine.gpu_load(right)
-  #     pln_mat2 = rpt_engine.gpu_decrypt(enc_mat2, pub_key, pvt_key)
-  #
-  #     #test [dotmul] [dotadd]
-  #     #res = rpt_engine.gpu_dotadd(enc_mat1, enc_mat2, pub_key)
-  #     res_mat = rpt_engine.gpu_dotmul(enc_mat1, pln_mat2, pub_key)
-  #
-  #     res_mat_pln = rpt_engine.gpu_decrypt(res_mat, pub_key, pvt_key)
-  #
-  #     #test [dump] [load]
-  #     h_Byte = rpt_engine.gpu_dump(res_mat_pln)
-  #     d_mem = rpt_engine.gpu_load(h_Byte)
-  #
-  #     res_val = rpt_engine.gpu_decode(d_mem, pub_key)
-  #
-  #     #mpz_result = rpt_engine.add(gpu_left_mpz, gpu_right_mpz, pub_key, pvt_key)
-  #     #return rpt_engine.gpu_dump(mpz_result)
-  #     return b'hello'
-  #
-  #   return self._store.join(other._store, lambda left, right : functor(left, right))
-  #   #return 1
-  #
-  # def gpu_load(self, other):
-  #   def functor(left, right):
-  #
-  #     #load
-  #     gpu_left_enc = rpt_engine.gpu_load(left)
-  #     gpu_right_enc = rpt_engine.gpu_load(right)
-  #
-  #     #dump
-  #
-  #     d_dump = rpt_engine.gpu_dump(gpu_left_enc)
-  #     d_load = rpt_engine.gpu_load(d_dump)
-  #
-  #     #mpz_result = rpt_engine.add(gpu_left_mpz, gpu_right_mpz, pub_key, pvt_key)
-  #     #return rpt_engine.gpu_dump(mpz_result)
-  #     return b'hello'
-  #
-  #   return self._store.join(other._store, lambda left, right : functor(left, right))
-  #   #return 1
+    #local function
+    def matmul_local(self, vec):
+        _engine = self._engine
+        def seq_op(mat, vec):
+            print("python mat_mul here")
+            mat_enc = _engine.load(mat)
+            vec_mng = _engine.num2Mng(vec)
+            res = _engine.matmul(mat_enc, vec_mng)
+            return _engine.dump(res)
+
+        return RollPaillierTensor(self._store.map_values(lambda v: seq_op(v, vec)))
+
+    def matmul_r_eql_local(self, vec):
+        _engine = self._engine
+        def seq_op(mat, vec):
+            mat_enc = _engine.load(mat)
+            res = _engine.matmul_r_eql(mat_enc, vec.data)
+            return _engine.dump(res)
+
+        return RollPaillierTensor(self._store.map_values(lambda v: seq_op(v, vec)))
+
+    def matmul_c_eql_local(self, vec):
+        _engine = self._engine
+        def seq_op(mat, vec):
+            print("python mat_mul_r_eql here")
+            mat_enc = _engine.load(mat)
+            res = _engine.matmul_c_eql(mat_enc, vec.data)
+            return _engine.dump(res)
+
+        return RollPaillierTensor(self._store.map_values(lambda v: seq_op(v, vec)))
+
+
+
+    def matmul_local_numpy(self, vec):
+        def seq_op(mat, vec):
+            # print("matmul_local_numpy : mat", mat)
+            # print("matmul_local_numpy : vec", vec)
+            res = mat.dot(vec)
+            print("[matmul_local]", res)
+            return res
+        return RollPaillierTensor(self._store.map_values(lambda v: seq_op(v, vec)))
