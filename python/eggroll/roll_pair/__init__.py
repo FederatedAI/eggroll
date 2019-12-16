974d5bdf9ffb2dc925e1f471c61c725d61ed9861
itboyljm <itboyljm@gmail.com>
2019-12-16 18:33:50 +0800

    refactor roll pair

---
+++ python/eggroll/roll_pair/__init__.py
@@ -0,0 +1,22 @@
+from eggroll.core.constants import SerdesTypes
+from eggroll.core.meta_model import ErPartition, ErStore
+from eggroll.core.pair_store import create_pair_adapter
+from eggroll.core.serdes.eggroll_serdes import PickleSerdes, \
+    CloudPickleSerdes, EmptySerdes
+from eggroll.roll_pair.utils.pair_utils import get_db_path
+
+
+def create_adapter(er_partition: ErPartition):
+    options = dict()
+    options['store_type'] = er_partition._store_locator._store_type
+    options ['path'] = get_db_path(er_partition)
+    return create_pair_adapter(options=options)
+
+
+def create_serdes(serdes_type: SerdesTypes = SerdesTypes.CLOUD_PICKLE):
+    if serdes_type == SerdesTypes.CLOUD_PICKLE or serdes_type == SerdesTypes.PROTOBUF:
+        return CloudPickleSerdes
+    elif serdes_type == SerdesTypes.PICKLE:
+        return PickleSerdes
+    else:
+        return EmptySerdes
