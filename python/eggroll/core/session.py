1bef67b16ebaab47c472321f7373d512a87bfa4b
maxwong <maxwong@live.cn>
2019-12-21 00:59:09 +0800

    1. Optimized default properties. 2. Remove local absolute paths. 3. Improved bash scripts.

--- python/eggroll/core/session.py
+++ python/eggroll/core/session.py
@@ -12,17 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import os
-import signal
-import threading
-import time
-from copy import copy
 
 from eggroll.core.client import ClusterManagerClient
 from eggroll.core.conf_keys import SessionConfKeys
-from eggroll.core.constants import SessionStatus, ProcessorStatus, \
-    ProcessorTypes
-from eggroll.core.meta_model import ErProcessor, ErSessionMeta, \
-    ErEndpoint, ErPartition
+from eggroll.core.constants import SessionStatus, ProcessorTypes
+from eggroll.core.meta_model import ErSessionMeta, \
+    ErPartition
 from eggroll.core.utils import get_self_ip, time_now
 
 # TODO:1: support windows
@@ -30,100 +25,8 @@ from eggroll.core.utils import get_self_ip, time_now
 if "EGGROLL_STANDALONE_DEBUG" not in os.environ:
     os.environ['EGGROLL_STANDALONE_DEBUG'] = "1"
 
-class StandaloneThread(threading.Thread):
-    def __init__(self, session_id="sid1", manager_port=4670, egg_port=20001, egg_transfer_port=20002):
-        threading.Thread.__init__(self)
-        self.setDaemon(True)
-        self.eggroll_home = "."
-        if "EGGROLL_HOME" in os.environ:
-            self.eggroll_home = os.environ["EGGROLL_HOME"]
-        else:
-            self.eggroll_home = "./"
-        self.boot = self.eggroll_home + "/bin/eggroll_boot.sh"
-        print("aa", self.boot)
-        self.standalone = f"{self.eggroll_home}/bin/eggroll_boot_standalone.sh -p \
-                          {manager_port} -e {egg_port} -t {egg_transfer_port} -s {session_id}"
-        self.pname = str(session_id) + "-standalone"
-
-    def run(self):
-        print("StandaloneThread start：" + self.name)
-        os.system(self.boot + " start '" + self.standalone + "' " + self.pname)
-        print("StandaloneThread stop：" + self.name)
-
-    def stop(self):
-        os.system(self.boot + " stop '" + self.standalone + "' " + self.pname)
-
-
 class ErDeploy:
     pass
-
-class ErStandaloneDeploy(ErDeploy):
-    def __init__(self, session_meta: ErSessionMeta, options={}):
-        self.manager_port = options.get("eggroll.standalone.manager.port", 4670)
-        self.egg_ports = [int(v) for v in options.get("eggroll.standalone.egg.ports", "20001").split(",")]
-        self.egg_transfer_ports = [int(v) for v in options.get("eggroll.standalone.egg.transfer.ports", "20002").split(",")]
-        self.self_server_node_id = int(options.get("eggroll.standalone.server.node.id", "2"))
-        self._eggs = {self.self_server_node_id:[]}
-        if len(self.egg_ports) > 1:
-            raise NotImplementedError()
-        if not ("EGGROLL_STANDALONE_DEBUG" in os.environ and os.environ['EGGROLL_STANDALONE_DEBUG'] == "1"):
-            self.standalone_thread = StandaloneThread(session_meta._id, self.manager_port, self.egg_ports[0])
-            self.standalone_thread.start()
-            print("standalone_thread start", self.standalone_thread)
-            signal.signal(signal.SIGTERM, self.stop)
-            signal.signal(signal.SIGINT, self.stop)
-            # TODO:0: more general
-            time.sleep(5)
-
-        self._eggs[self.self_server_node_id].append(ErProcessor(id=1,
-                                         server_node_id=self.self_server_node_id,
-                                         processor_type=ProcessorTypes.EGG_PAIR,
-                                         status=ProcessorStatus.RUNNING,
-                                         command_endpoint=ErEndpoint("localhost", self.egg_ports[0]),
-                                         transfer_endpoint=ErEndpoint("localhost", self.egg_transfer_ports[0])))
-
-        self._rolls = [ErProcessor(id=1,
-                                   server_node_id=self.self_server_node_id,
-                                   processor_type=ProcessorTypes.ROLL_PAIR_MASTER,
-                                   status=ProcessorStatus.RUNNING,
-                                   command_endpoint=ErEndpoint("localhost", self.manager_port))]
-
-        self._processors = [self._rolls[0]] + list(self._eggs[self.self_server_node_id])
-
-        session_meta_with_processor = copy(session_meta)
-        session_meta_with_processor._processors = self._processors
-        self.cm_client = ClusterManagerClient(options=options)
-
-        self.cm_client.register_session(session_meta_with_processor)
-
-    def stop(self):
-        if self.standalone_thread:
-            self.standalone_thread.stop()
-
-
-class ErClusterDeploy(ErDeploy):
-    def __init__(self, session_meta: ErSessionMeta, options={}):
-        self.cm_client = ClusterManagerClient(options=options)
-        self.session_meta = session_meta
-        print(f'session_meta: {session_meta}')
-        processor_batch = self.cm_client.get_or_create_session(self.session_meta)
-
-        self._rolls = list()
-        self._eggs = dict()
-        for processor in processor_batch._processors:
-            processor_type = processor._processor_type
-            if processor_type == ProcessorTypes.EGG_PAIR:
-                node_id = processor._server_node_id
-                if node_id not in self._eggs.keys():
-                    self._eggs[node_id] = list()
-
-                node_eggs = self._eggs.get(node_id)
-                node_eggs.append(processor)
-            elif processor_type == ProcessorTypes.ROLL_PAIR_MASTER:
-                self._rolls.append(processor)
-            else:
-                raise ValueError(f'processor type {processor_type} is unknown')
-
 
 class ErSession(object):
     def __init__(self,
@@ -199,11 +102,3 @@ class ErSession(object):
 
     def get_all_options(self):
         return self.__options.copy()
-
-
-
-
-
-
-
-
