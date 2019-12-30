22efae479b3fe66f7249dc7408f1f583d5edd7e4
zzzcq <39122697+zzzcq@users.noreply.github.com>
2019-12-30 19:53:53 +0800

    update eggroll.sh
    change the format of eggroll.sh

--- bin/eggroll.sh
+++ bin/eggroll.sh
@@ -132,14 +132,16 @@ start() {
     getpid
     if [[ $? -eq 1 ]]; then
 		mklogsdir
+		
 		java -Dlog4j.configurationFile=${EGGROLL_HOME}/conf/log4j2.properties -cp ${EGGROLL_HOME}/lib/*: com.webank.eggroll.core.Bootstrap --bootstraps ${main_class} -c ${EGGROLL_HOME}/conf/eggroll.properties -p $port -s EGGROLL_DAEMON >> ${EGGROLL_HOME}/logs/${module}_console.log 2>>${EGGROLL_HOME}/logs/${module}_error.log &
+		
 		echo $!>${module}_pid
 		getpid
 		if [[ $? -eq 0 ]]; then
 			echo "service start sucessfully. pid: ${pid}"
-		else
+        else
 			echo "service start failed"
-		fi
+        fi
     else
         echo "service already started. pid: ${pid}"
     fi
@@ -154,7 +156,7 @@ stop() {
 		sleep 1
 		getpid
         if [[ $pid -eq $module_pid ]]; then
-			echo "kill error"
+            echo "kill error"
         else
 			echo "killed"
 			echo "999999" >${module}_pid
