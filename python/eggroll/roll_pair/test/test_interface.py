30b10757ac91ac68c23a47b774c9f1c1d54aadab
maxwong <maxwong@live.cn>
2019-12-30 14:22:22 +0800

    1. Supports multi partition tests

--- python/eggroll/roll_pair/test/test_interface.py
+++ python/eggroll/roll_pair/test/test_interface.py
@@ -19,9 +19,9 @@ from eggroll.roll_pair.test.roll_pair_test_assets import get_debug_test_context,
     get_cluster_context, get_standalone_context, set_default_option, \
     get_default_options
 
-is_debug = True
+is_debug = False
 is_standalone = False
-total_partitions = 3
+total_partitions = 1
 
 
 def kv_generator(limit):
@@ -57,15 +57,17 @@ class TestStandalone(unittest.TestCase):
 
 
     def test_get(self):
+        options = get_default_options()
+        options['include_key'] = True
         for i in range(10):
-            self.ctx.load("ns1", "n26").put(f"k{i}", f"v{i}")
-            print(self.ctx.load("ns1", "n26").get(f"k{i}"))
+            self.ctx.load("ns1", "testGet", options=options).put(f"k{i}", f"v{i}")
+            print(self.ctx.load("ns1", "testGet").get(f"k{i}"))
 
     def test_put_all(self):
         #data = [("k1", "v1"), ("k2", "v2"), ("k3", "v3"), ("k4", "v4"), ("k5", "v5"), ("k6", "v6")]
         #data = [("k1", "v1"), ("k2", "v2")]
         options = get_default_options()
-        t = self.ctx.load("ns1", "testPutAll")
+        t = self.ctx.load("ns1", "testPutAll", options=options)
         options['include_key'] = True
         t.put_all(kv_generator(100), options=options)
 
@@ -73,9 +75,11 @@ class TestStandalone(unittest.TestCase):
         print(list(t.get_all()))
 
     def test_put_all_value(self):
-        self.ctx.load("ns1", "testPutAll2").destroy()
-        cnt = 100*1000
-        rp = self.ctx.load("ns1", "testPutAll2").put_all(("s" for i in range(100*1000)), options={"include_key": False})
+        options = get_default_options()
+        self.ctx.load("ns1", "testPutAllValue", options=options).destroy()
+        options['include_key'] = False
+        cnt = 100
+        rp = self.ctx.load("ns1", "testPutAllValue", options=options).put_all(("s" for i in range(cnt)), options=options)
         self.assertEqual(rp.count(), cnt)
 
     def test_multi_partition_put_all(self):
@@ -88,7 +92,8 @@ class TestStandalone(unittest.TestCase):
         print(table.count())
 
     def test_get_all(self):
-        table =self.ctx.load("ns1", "testMultiPartitionPutAll")
+        options = get_default_options()
+        table =self.ctx.load("ns1", "testMultiPartitionPutAll", options=options)
         print(str(table))
         res = table.get_all()
         print(list(res))
@@ -103,7 +108,6 @@ class TestStandalone(unittest.TestCase):
 
     def test_delete(self):
         options = get_default_options()
-        options['total_partitions'] = 1
         options['include_key'] = True
         data = [("k1", "v1"), ("k2", "v2"), ("k3", "v3"), ("k4", "v4")]
         table = self.ctx.load('ns1', 'test_delete_one', options=options).put_all(data, options=options)
@@ -113,7 +117,6 @@ class TestStandalone(unittest.TestCase):
 
     def test_destroy(self):
         options = get_default_options()
-        options['total_partitions'] = 1
         options['include_key'] = True
         data = [("k1", "v1"), ("k2", "v2"), ("k3", "v3"), ("k4", "v4")]
         table = self.ctx.load('ns1', 'test_destroy', options=options).put_all(data, options=options)
@@ -124,37 +127,35 @@ class TestStandalone(unittest.TestCase):
 
     def test_take(self):
         options = get_default_options()
-        options['total_partitions'] = 1
         options['keys_only'] = True
         table = self.ctx.load('ns1', 'test_take', options=options).put_all(range(10), options=options)
         print(table.take(n=3, options=options))
 
         options_kv = get_default_options()
-        options_kv['total_partitions'] = 1
         options_kv['keys_only'] = False
         table = self.ctx.load('ns1', 'test_take_kv', options=options_kv).put_all(range(10), options=options_kv)
         print(table.take(n=3, options=options_kv))
 
     def test_first(self):
         options = get_default_options()
-        options['total_partitions'] = 1
         options['keys_only'] = True
         table = self.ctx.load('ns1', 'test_take', options=options).put_all(range(10), options=options)
         print(table.first(options=options))
 
         options_kv = get_default_options()
-        options_kv['total_partitions'] = 1
         options_kv['keys_only'] = False
         table = self.ctx.load('ns1', 'test_take_kv', options=options_kv).put_all(range(10), options=options_kv)
         print(table.first(options=options_kv))
 
     def test_map_values(self):
-        rp = self.ctx.load("ns1", "test_map_values").put_all(range(10), options=get_default_options())
+        options = get_default_options()
+        rp = self.ctx.load("ns1", "test_map_values", options=options).put_all(range(10), options=options)
         print(list(rp.map_values(lambda v: str(v) + 'map_values').get_all()))
 
     def test_map_partitions(self):
+        options = get_default_options()
         data = [(str(i), i) for i in range(10)]
-        rp = self.ctx.load("ns1", "test_map_partitions").put_all(data, options={"include_key": True})
+        rp = self.ctx.load("ns1", "test_map_partitions", options=options).put_all(data, options={"include_key": True})
         def func(iter):
             ret = []
             for k, v in iter:
@@ -167,12 +168,13 @@ class TestStandalone(unittest.TestCase):
         # rp = self.ctx.load("ns1", "testMap2")
         # rp.destroy()
         # rp = self.ctx.load("ns1", "testMap2").put_all("s"*4000 for i in range(100*1000))
-        rp = self.ctx.load("ns1", "testMap2")
+        options = get_default_options()
+        rp = self.ctx.load("ns1", "testMap2", options=options)
         # rp = self.ctx.load("ns1", "testMap3", {"store_type":StoreTypes.ROLLPAIR_CACHE})
         # rp.put_all(range(100*1000))
         # print(rp.count())
         # print(rp.map_values(lambda v: v))
-        print(rp.map(lambda k, v: (k + 1, v)))
+        print(rp.map(lambda k, v: (k + 1, v)).count())
         # print(rp.first())
         # print(rp.map(lambda k, v: (k + 1, v)).count())
 
@@ -188,7 +190,8 @@ class TestStandalone(unittest.TestCase):
         print(result.count())
 
     def test_collapse_partitions(self):
-        rp = self.ctx.load("ns1", "test_collapse_partitions").put_all(range(5), options=get_default_options())
+        options = get_default_options()
+        rp = self.ctx.load("ns1", "test_collapse_partitions", options=options).put_all(range(5), options=options)
         def f(iterator):
             sum = []
             for k, v in iterator:
@@ -197,11 +200,13 @@ class TestStandalone(unittest.TestCase):
         print(list(rp.collapse_partitions(f).get_all()))
 
     def test_filter(self):
-        rp = self.ctx.load("ns1", "test_filter").put_all(range(5), options=get_default_options())
+        options = get_default_options()
+        rp = self.ctx.load("ns1", "test_filter", options=options).put_all(range(5), options=options)
         print(list(rp.filter(lambda k, v: v % 2 != 0).get_all()))
 
     def test_flatMap(self):
-        rp = self.ctx.load("ns1", "test_flat_map").put_all(range(5), options=get_default_options())
+        options = get_default_options()
+        rp = self.ctx.load("ns1", "test_flat_map", options=options).put_all(range(5), options=options)
         import random
         def foo(k, v):
             result = []
@@ -212,48 +217,51 @@ class TestStandalone(unittest.TestCase):
         print(list(rp.flat_map(foo).get_all()))
 
     def test_glom(self):
-        rp = self.ctx.load("ns1", "test_glom").put_all(range(5), options=get_default_options())
+        options = get_default_options()
+        rp = self.ctx.load("ns1", "test_glom", options=options).put_all(range(5), options=options)
         print(list(rp.glom().get_all()))
 
     def test_join(self):
-        left_rp = self.ctx.load("ns1", "testJoinLeft").put_all([('a', 1), ('b', 4)], options={"include_key": True})
-        right_rp = self.ctx.load("ns1", "testJoinRight").put_all([('a', 2), ('c', 4)], options={"include_key": True})
+        options = get_default_options()
+        left_rp = self.ctx.load("ns1", "testJoinLeft", options=options).put_all([('a', 1), ('b', 4)], options={"include_key": True})
+        right_rp = self.ctx.load("ns1", "testJoinRight", options=options).put_all([('a', 2), ('c', 4)], options={"include_key": True})
         print(list(left_rp.join(right_rp, lambda v1, v2: v1 + v2).get_all()))
 
     def test_reduce(self):
         from operator import add
-        rp = self.ctx.load("ns1", "testReduce").put_all(range(20), options=get_default_options())
+        options = get_default_options()
+        rp = self.ctx.load("ns1", "testReduce", options=options).put_all(range(20), options=options)
         print(list(rp.reduce(add).get_all()))
 
     def test_multi_partition_reduce(self):
         from operator import add
         options = get_default_options()
-        options['total_partitions'] = 3
-        options['include_key'] = True
         rp = self.ctx.load("ns1", "testMultiPartitionReduce", options=options).put_all(range(20), options=options)
         print(list(rp.reduce(add).get_all()))
 
     def test_sample(self):
-        rp = self.ctx.load("ns1", "testSample").put_all(range(100), options=get_default_options())
+        options = get_default_options()
+        rp = self.ctx.load("ns1", "testSample", options=options).put_all(range(100), options=get_default_options())
         print(6 <= rp.sample(0.1, 81).count() <= 14)
 
     def test_subtract_by_key(self):
-        left_rp = self.ctx.load("namespace1206", "testSubtractByKeyLeft1206").put_all(range(10), options=get_default_options())
-        right_rp = self.ctx.load("namespace1206", "testSubtractByKeyRight1206").put_all(range(5), options=get_default_options())
+        options = get_default_options()
+        left_rp = self.ctx.load("namespace1206", "testSubtractByKeyLeft1206", options=options).put_all(range(10), options=options)
+        right_rp = self.ctx.load("namespace1206", "testSubtractByKeyRight1206", options=options).put_all(range(5), options=options)
         print(list(left_rp.subtract_by_key(right_rp).get_all()))
 
     def test_union(self):
         options = get_default_options()
-        left_rp = self.ctx.load("ns1", "testUnionLeft").put_all([1, 2, 3], options=options)
+        left_rp = self.ctx.load("ns1", "testUnionLeft", options=options).put_all([1, 2, 3], options=options)
 
         options['include_key'] = True
-        right_rp = self.ctx.load("ns1", "testUnionRight").put_all([(1, 1), (2, 2), (3, 3)])
+        right_rp = self.ctx.load("ns1", "testUnionRight", options=options).put_all([(1, 1), (2, 2), (3, 3)])
         print(list(left_rp.union(right_rp, lambda v1, v2: v1 + v2).get_all()))
 
         options = get_default_options()
-        left_rp = self.ctx.load("namespace1206", "testUnionLeft1206").put_all([1, 2, 3])
+        left_rp = self.ctx.load("namespace1206", "testUnionLeft1206", options=options).put_all([1, 2, 3], options=options)
         options['include_key'] = True
-        right_rp = self.ctx.load("namespace1206", "testUnionRight1206").put_all([(1, 1), (2, 2), (3, 3)], options=options)
+        right_rp = self.ctx.load("namespace1206", "testUnionRight1206", options=options).put_all([(1, 1), (2, 2), (3, 3)], options=options)
         print("left:", list(left_rp.get_all()))
         print("right:", list(right_rp.get_all()))
         print(list(left_rp.union(right_rp, lambda v1, v2: v1 + v2).get_all()))
