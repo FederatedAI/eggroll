963319e2fe0d36b4b8dfd8eeb7724a12f8b145dc
wenzanhuang <154000436@qq.com>
2020-01-02 16:31:06 +0800

    standalone mode adapt FATE api passed

--- python/eggroll/roll_pair/test/roll_pair_test_assets.py
+++ python/eggroll/roll_pair/test/roll_pair_test_assets.py
@@ -29,17 +29,17 @@ ER_STORE1 = ErStore(
                                  name="name"))
 
 
-def get_debug_test_context(is_standalone=False):
-    manager_port = 4670
-    egg_ports = [20001]
-    egg_transfer_ports = [20002]
+def get_debug_test_context(is_standalone=False, manager_port=4670, egg_port=20001, transfer_port=20002, session_id='testing'):
+    manager_port = manager_port
+    egg_ports = [egg_port]
+    egg_transfer_ports = [transfer_port]
     self_server_node_id = 2
 
     options = {}
     if is_standalone:
         options[SessionConfKeys.CONFKEY_SESSION_DEPLOY_MODE] = "standalone"
     options[TransferConfKeys.CONFKEY_TRANSFER_SERVICE_HOST] = "localhost"
-    options[TransferConfKeys.CONFKEY_TRANSFER_SERVICE_PORT] = "20002"
+    options[TransferConfKeys.CONFKEY_TRANSFER_SERVICE_PORT] = str(transfer_port)
     options[ClusterManagerConfKeys.CONFKEY_CLUSTER_MANAGER_PORT] = str(manager_port)
     options[NodeManagerConfKeys.CONFKEY_NODE_MANAGER_PORT] = str(manager_port)
 
@@ -57,7 +57,7 @@ def get_debug_test_context(is_standalone=False):
                        status=ProcessorStatus.RUNNING,
                        command_endpoint=ErEndpoint("localhost", manager_port))
 
-    session = ErSession(session_id='testing',
+    session = ErSession(session_id,
                         processors=[egg, roll],
                         options=options)
     # session = ErSession(options={})
