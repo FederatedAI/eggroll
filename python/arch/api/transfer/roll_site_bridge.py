0129e00efe5c4dcc78678417edcabe77246a3948
wenzanhuang <154000436@qq.com>
2020-01-02 17:08:17 +0800

    standalone mode adapt FATE api passed

--- python/arch/api/transfer/roll_site_bridge.py
+++ python/arch/api/transfer/roll_site_bridge.py
@@ -4,25 +4,40 @@ from arch.api.table.session import FateSession
 from arch.api.transfer import Rubbish, Party, Federation
 from typing import Union
 
-from eggroll.utils import log_utils
 from eggroll.roll_pair.roll_pair import RollPair
 from eggroll.roll_site.roll_site import RollSite
-log_utils.setDirectory()
-LOGGER = log_utils.getLogger()
 
 OBJECT_STORAGE_NAME = "__federation__"
 STATUS_TABLE_NAME = "__status__"
 
-def init_roll_site_context():
+from eggroll.utils import log_utils
+LOGGER = log_utils.get_logger()
+
+def init_roll_site_context(runtime_conf, session_id):
     from eggroll.roll_site.roll_site import RollSiteContext
     from eggroll.roll_pair.roll_pair import RollPairContext
-    options = {'runtime_conf_path': 'python/eggroll/roll_site/conf/role_conf.json',
+    LOGGER.info("runtime_conf: {}".format(runtime_conf))
+    default_opts = {'runtime_conf_path': 'python/eggroll/roll_site/conf/role_conf.json',
                'server_conf_path': 'python/eggroll/roll_site/conf/server_conf.json',
                'transfer_conf_path': 'python/eggroll/roll_site/conf/transfer_conf.json'}
+    if runtime_conf.get("local").get("role") == "host":
+        LOGGER.info("init_roll_site_context ----host")
+        options = default_opts
+    elif runtime_conf.get("local").get("role") == "guest":
+        LOGGER.info("init_roll_site_context ----guest")
+        options = {'runtime_conf_path': 'python/eggroll/roll_site/conf_guest/role_conf.json',
+                   'server_conf_path': 'python/eggroll/roll_site/conf_guest/server_conf.json',
+                   'transfer_conf_path': 'python/eggroll/roll_site/conf_guest/transfer_conf.json'}
+    else:
+        options = default_opts
+
     session_instance = FateSession.get_instance()._eggroll.get_session()
     rp_context = RollPairContext(session_instance)
-    rs_context = RollSiteContext(session_instance.get_session_id(), options=options, rp_ctx=rp_context)
+
+    LOGGER.info("init session_id:{}".format(session_instance.get_session_id()))
+    rs_context = RollSiteContext(session_id, options=options, rp_ctx=rp_context)
     return rp_context, rs_context
+
 
 async def check_status_and_get_value(_table, _key):
     _value = _table.get(_key)
@@ -33,16 +48,20 @@ async def check_status_and_get_value(_table, _key):
     LOGGER.debug("[GET] Got {} type {}".format(_key, 'Table' if isinstance(_value, tuple) else 'Object'))
     return _value
 
+
 def _remote__object_key(*args):
     LOGGER.debug("args123:{}".format(args))
     return "-".join(["{}".format(arg) for arg in args])
 
+
 class FederationRuntime(Federation):
 
     def __init__(self, session_id, runtime_conf):
+        LOGGER.info("session_id:{}".format(session_id))
         super().__init__(session_id, runtime_conf)
-        self.rpc, self.rsc = init_roll_site_context()
+        self.rpc, self.rsc = init_roll_site_context(runtime_conf, session_id)
         self._loop = asyncio.get_event_loop()
+        self.role = runtime_conf.get("local").get("role")
 
     def _send(self, name: str, tag: str, dst_party: Party, rubbish: Rubbish, table: RollPair, obj=None):
         tagged_key = f"{name}-{tag}"
@@ -91,20 +110,29 @@ class FederationRuntime(Federation):
         return rtn, rubbish
 
     def get(self, name, tag, parties: Union[Party, list]):
+        if isinstance(parties, Party):
+            parties = [parties]
+
         rubbish = Rubbish(name, tag)
         rtn = []
         rs = self.rsc.load(name=name, tag=tag)
-        futures = rs.pull(parties=parties)
+        rs_parties = []
+        for party in parties:
+            rs_parties.append((party.role, party.party_id))
+
+        LOGGER.info("rs_parties:{}".format(rs_parties))
+        futures = rs.pull(parties=rs_parties)
+        from arch.api.table.eggroll2.table_impl import DTable
         for future in futures:
             obj = future.result()
             if isinstance(obj, RollPair):
-                rtn.append(obj)
+                rtn.append(DTable.from_dtable(obj.ctx.get_session().get_session_id(),obj))
                 rubbish.add_table(obj)
                 LOGGER.info("RollPair:".format(obj))
             else:
                 rtn.append(obj)
                 LOGGER.info("obj:".format(obj))
-        return rtn ,rubbish
+        return rtn, rubbish
 
     def remote_local(self, obj, name: str, tag: str, parties: Union[Party, list]) -> Rubbish:
         if isinstance(parties, Party):
@@ -138,21 +166,17 @@ class FederationRuntime(Federation):
         return rubbish
 
     def remote(self, obj, name, tag, parties):
-        if True:
-            LOGGER.info("standalone federation remote")
-            return self.remote_local(obj=obj, name=name, tag=tag, parties=parties)
-
         if isinstance(parties, Party):
             parties = [parties]
-        self._remote_side_auth(name=name, parties=parties)
-        rubbish = Rubbish(name, tag)
 
-        # if obj is a dtable, remote it
-        if isinstance(obj, RollPair):
-            obj.set_gc_disable()
-            for party in parties:
-                log_msg = f"src={self.local_party}, dst={party}, name={name}, tag={tag}, session_id={self._session_id}"
-                LOGGER.debug(f"[REMOTE] sending table: {log_msg}")
-                self._send(name=name, tag=tag, dst_party=party, rubbish=rubbish, table=obj)
-                LOGGER.debug(f"[REMOTE] table done: {log_msg}")
-            return rubbish
+        rs_parties = []
+        for party in parties:
+            rs_parties.append((party.role, party.party_id))
+
+        rubbish = Rubbish(name=name, tag=tag)
+        rs = self.rsc.load(name=name, tag=tag)
+        futures = rs.push(obj=obj, parties=rs_parties)
+        for future in futures:
+            role, party = future.result()
+            LOGGER.info("RollSite push role:{} party:{}".format(role, party))
+        return rubbish
