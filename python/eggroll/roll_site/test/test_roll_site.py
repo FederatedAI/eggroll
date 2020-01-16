526fbeeb04f69366b1b64329d49b2f6c1cf6ed6e
maxwong <maxwong@live.cn>
2020-01-16 15:53:55 +0800

    1. Added standalone test support. 2. Modifies retry log in roll site.

--- python/eggroll/roll_site/test/test_roll_site.py
+++ python/eggroll/roll_site/test/test_roll_site.py
@@ -18,7 +18,8 @@ import unittest
 
 from eggroll.roll_pair.roll_pair import RollPair
 from eggroll.roll_site.test.roll_site_test_asset import get_debug_test_context, \
-    get_cluster_context, default_props_file, remote_parties, \
+    get_cluster_context, get_standalone_context, default_props_file, \
+    remote_parties, \
     get_parties
 
 props_file_host = default_props_file
@@ -52,8 +53,8 @@ class TestRollSiteBase(unittest.TestCase):
         rs = self.rs_context_guest.load(name=self._obj_rs_name, tag=self._obj_rs_tag)
         futures = rs.push(self._obj, remote_parties)
         for future in futures:
-            role, party = future.result()
-            print("result:", role, party)
+            result = future.result()
+            print("result:", result)
 
     def test_get(self):
         rs = self.rs_context_host.load(name=self._obj_rs_name, tag=self._obj_rs_tag)
@@ -75,8 +76,8 @@ class TestRollSiteBase(unittest.TestCase):
         rs = self.rs_context_guest.load(name=self._rp_rs_name, tag=self._rp_rs_tag)
         futures = rs.push(rp, remote_parties)
         for future in futures:
-            role, party = future.result()
-            print("result:", role, party)
+            result = future.result()
+            print("result: ", result)
 
     def test_get_rollpair(self):
         rs = self.rs_context_host.load(name=self._rp_rs_name, tag=self._rp_rs_tag)
@@ -103,6 +104,26 @@ class TestRollSiteBase(unittest.TestCase):
 
         rp = rp_context.load('atest', f'__federation__-atest-{self._rp_rs_name}-{self._rp_rs_tag}-10002-host-10001')
         print(list(rp.get_all()))
+
+
+
+class TestRollSiteStandalone(TestRollSiteBase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.rs_context_host = get_standalone_context(props_file=props_file_host)
+        cls.rs_context_guest = get_standalone_context(props_file=props_file_guest)
+
+    def test_remote(self):
+        super().test_remote()
+
+    def test_get(self):
+        super().test_get()
+
+    def test_remote_rollpair(self):
+        super().test_remote_rollpair()
+
+    def test_get_rollpair(self):
+        super().test_get_rollpair()
 
 
 class TestRollSiteCluster(TestRollSiteBase):
