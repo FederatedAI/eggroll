39431febe304023bd3eefcd80ae904a41dbfb3f4
drougon <drougon@qq.com>
2020-01-18 17:29:32 +0800

    add: session stop bridge

--- python/arch/api/table/eggroll2/session_impl.py
+++ python/arch/api/table/eggroll2/session_impl.py
@@ -37,6 +37,7 @@ class FateSessionImpl(TableManger):
         self._eggroll = runtime_init(session=eggroll_session)
         self._session_id = eggroll_session.get_session_id()
         self._persistent_engine = persistent_engine
+        self._session = eggroll_session
         TableManger.set_instance(self)
 
     def get_persistent_engine(self):
@@ -102,5 +103,5 @@ class FateSessionImpl(TableManger):
         return self._session_id
 
     def stop(self):
-        return
+        return self._session.stop()
         #self._eggroll.stop()
