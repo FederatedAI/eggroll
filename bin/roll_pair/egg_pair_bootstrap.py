93ec6f3fa23bc5d5d44836ec4f6ccae0d3490008
hmoster <hwz_mjlwz@163.com>
2020-04-02 11:50:48 +0800

    support windows for the container.scala

--- bin/roll_pair/egg_pair_bootstrap.py
+++ bin/roll_pair/egg_pair_bootstrap.py
@@ -1,8 +1,6 @@
 import os
 from subprocess import Popen, PIPE
 import time
-import sys
-
 import struct
 from socket import *
 
@@ -26,7 +24,7 @@ def send_stop(pid):
     print("send data ok !")
 
 def get_property(config_file, property_name):
-  with open(config_file) as i_file: #Open the file
+  with open(config_file) as i_file:
     values = {}
 
     for line in i_file:
@@ -44,14 +42,7 @@ def get_property(config_file, property_name):
 
 
 def start(config_file, session_id, server_node_id, processor_id, port, transfer_port, pname):
-
     print('sub id ：',os.getpid(),'parent id ：',os.getppid())
-
-    pid = os.getpid()
-    pname_pid = 'bin/' + 'pid/' + pname + '.pid'
-    with open(pname_pid, 'w') as fp:
-        fp.write(str(pid))
-        fp.close()
         
     if session_id is None:
       print("session-id is blank")
@@ -64,10 +55,8 @@ def start(config_file, session_id, server_node_id, processor_id, port, transfer_
     if transfer_port is 0 and port is not 0:
       transfer_port = port
 
-    #venv = get_property(config_file, "eggroll.resourcemanager.bootstrap.egg_pair.venv")
-    #pythonpath = get_property(config_file, "eggroll.resourcemanager.bootstrap.egg_pair.pythonpath")
+    pythonpath = get_property(config_file, "eggroll.resourcemanager.bootstrap.egg_pair.pythonpath")
     filepath = get_property(config_file, "eggroll.resourcemanager.bootstrap.egg_pair.filepath")
-    #logs_dir = get_property(config_file, "eggroll.logs.dir")
     node_manager_port = get_property(config_file, "eggroll.resourcemanager.nodemanager.port")
     cluster_manager_host = get_property(config_file, "eggroll.resourcemanager.clustermanager.host")
     cluster_manager_port = get_property(config_file, "eggroll.resourcemanager.clustermanager.port")
@@ -89,8 +78,8 @@ def start(config_file, session_id, server_node_id, processor_id, port, transfer_
 
     os.environ['EGGROLL_LOG_FILE'] ="egg_pair-" + processor_id
 
-
-    cmd = 'python ' + ' ../' + filepath + ' --config ' + config_file + \
+    cmd = pythonpath + ' ' + filepath + \
+          ' --config ' + config_file + \
           ' --session-id ' + session_id + \
           ' --server-node-id ' + server_node_id + \
           ' --cluster-manager ' + cluster_manager_host + ':' + cluster_manager_port + \
@@ -113,37 +102,21 @@ def start(config_file, session_id, server_node_id, processor_id, port, transfer_
 
     log_file = os.path.join(eggroll_logs_dir, eggroll_log_file)
     err_file = os.path.join(eggroll_logs_dir, eggroll_err_file)
-    cmd = cmd + ' >> ' + log_file + ' 2> ' + err_file + ' &'
 
-    '''
-    argument = ' --config ' + config_file + \
-               ' --session-id ' + session_id + \
-               ' --server-node-id ' + server_node_id + \
-               ' --cluster-manager ' + cluster_manager_host + ':' + cluster_manager_port + \
-               ' --node-manager ' + node_manager_port + ' --processor-id ' + processor_id
-    '''
-    #argument = '--config ../conf/eggroll.properties'
-    '''
-    argument = '...'
-    proc = Popen(['python', 'roll_pair/bar.py --config ../conf/eggroll.properties --session-id testing'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+    log_file_fp = open(log_file, 'ab')
+    err_file_fp = open(err_file, 'ab')
+    proc = Popen(cmd, shell=False, stdout=log_file_fp, stderr=err_file_fp)
     pid = proc.pid
-    output = proc.stdout.read()
-    print(output)
+
     print("pid:", pid)
-    '''
-    #os.system("python roll_pair/bar.py --config ../conf/eggroll.properties --session-id testing --node-manager 4670 >> logs/egg_pair-100.out 2> logs/egg_pair-100.err") #--server-node-id 1001 --cluster-manager 127.0.0.1:4670  --processor-id 100  &")
-
-    #sys.path.append('F:\webank\eggroll_2\eggroll\python')
-    os.system(cmd)
-    #egg_pair(config, session_id, server_node_id, cluster_manager_host,
-    #         cluster_manager_port, node_manager_port, processor_id)
-
-    #print("egg_pair processor id:", processor_id, "os process id:" >> ${EGGROLL_LOGS_DIR}/pid.txt
+    pname_pid = 'bin/' + 'pid/' + pname + '.pid'
+    with open(pname_pid, 'w') as fp:
+        fp.write(str(pid))
+        fp.close()
 
 def stop(pid):
     send_stop(pid)
-    '''
+    time.sleep(3)
     cmd = 'taskkill /pid ' + pid
     print(cmd)
     os.system(cmd)
-    '''
--- bin/roll_pair/roll_pair_master_bootstrap.py
+++ bin/roll_pair/roll_pair_master_bootstrap.py
@@ -1,8 +1,6 @@
 import os
 from subprocess import Popen, PIPE
 import time
-
-import struct
 from socket import *
 
 
@@ -33,14 +31,7 @@ def get_property(config_file, property_name):
 
 
 def start(config_file, session_id, server_node_id, processor_id, port, transfer_port, pname):
-
-  print('sub id ：',os.getpid(),'parent id ：',os.getppid())
-
-  pid = os.getpid()
-  pname_pid = 'bin/' + 'pid/' + pname + '.pid'
-  #os.mkdir('pid')
-  with open(pname_pid, 'w') as fp:
-    fp.write(str(pid))
+  print('roll_pair_master sub id ：', os.getpid(), 'parent id ：', os.getppid())
 
   if session_id is None:
     print("session-id is blank")
@@ -54,9 +45,6 @@ def start(config_file, session_id, server_node_id, processor_id, port, transfer_
     transfer_port = port
 
   #venv = get_property(config_file, "eggroll.resourcemanager.bootstrap.egg_pair.venv")
-  #pythonpath = get_property(config_file, "eggroll.resourcemanager.bootstrap.egg_pair.pythonpath")
-  filepath = get_property(config_file, "eggroll.resourcemanager.bootstrap.egg_pair.filepath")
-  #logs_dir = get_property(config_file, "eggroll.logs.dir")
   node_manager_port = get_property(config_file, "eggroll.resourcemanager.nodemanager.port")
   cluster_manager_host = get_property(config_file, "eggroll.resourcemanager.clustermanager.host")
   cluster_manager_port = get_property(config_file, "eggroll.resourcemanager.clustermanager.port")
@@ -126,37 +114,24 @@ def start(config_file, session_id, server_node_id, processor_id, port, transfer_
 
   log_file = os.path.join(eggroll_logs_dir, eggroll_log_file)
   err_file = os.path.join(eggroll_logs_dir, eggroll_err_file)
-  cmd = cmd + ' >> ' + log_file + ' 2> ' + err_file + ' &'
+  print(cmd)
 
-  '''
-  argument = ' --config ' + config_file + \
-             ' --session-id ' + session_id + \
-             ' --server-node-id ' + server_node_id + \
-             ' --cluster-manager ' + cluster_manager_host + ':' + cluster_manager_port + \
-             ' --node-manager ' + node_manager_port + ' --processor-id ' + processor_id
-  '''
-  #argument = '--config ../conf/eggroll.properties'
-  '''
-  argument = '...'
-  proc = Popen(['python', 'roll_pair/bar.py --config ../conf/eggroll.properties --session-id testing'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+  log_file_fp = open(log_file, 'ab')
+  err_file_fp = open(err_file, 'ab')
+  proc = Popen(cmd, shell=False, stdout=log_file_fp, stderr=err_file_fp)
   pid = proc.pid
-  output = proc.stdout.read()
-  print(output)
-  print("pid:", pid)
-  '''
-  #os.system("python roll_pair/bar.py --config ../conf/eggroll.properties --session-id testing --node-manager 4670 >> logs/egg_pair-100.out 2> logs/egg_pair-100.err") #--server-node-id 1001 --cluster-manager 127.0.0.1:4670  --processor-id 100  &")
-  os.system(cmd)
-  #egg_pair(config, session_id, server_node_id, cluster_manager_host,
-  #         cluster_manager_port, node_manager_port, processor_id)
 
-  #print("egg_pair processor id:", processor_id, "os process id:" >> ${EGGROLL_LOGS_DIR}/pid.txt
+  print("pid:", pid)
+  pname_pid = 'bin/' + 'pid/' + pname + '.pid'
+  with open(pname_pid, 'w') as fp:
+    fp.write(str(pid))
+    fp.close()
 
 
 def stop(pid):
   send_stop(pid)
 
-  '''
+  time.sleep(3)
   cmd = 'taskkill /pid ' + pid + ' -f'
   print(cmd)
   os.system(cmd)
-  '''
