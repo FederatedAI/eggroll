09cfe4f0451bad5650ab4693fa13a7401aad0e0a
hmoster <hwz_mjlwz@163.com>
2020-05-09 14:34:24 +0800

    add Micro definition: EGGROLL_PUSH_OBJ_WITH_ROLL_PAIR

--- jvm/roll_site/main/java/com/webank/eggroll/rollsite/grpc/service/DataTransferPipedServerImpl.java
+++ jvm/roll_site/main/java/com/webank/eggroll/rollsite/grpc/service/DataTransferPipedServerImpl.java
@@ -328,7 +328,7 @@ public class DataTransferPipedServerImpl extends DataTransferServiceGrpc.DataTra
                 return;
             }
 
-            if (System.getenv("PUSH_OBJ_WITH_ROLL_PAIR") != null && System.getenv("PUSH_OBJ_WITH_ROLL_PAIR").equals("TRUE")) {
+            if (System.getenv("EGGROLL_PUSH_OBJ_WITH_ROLL_PAIR").equals("1")) {
                 Pipe pipe = new PacketQueueSingleResultPipe();
                 LOGGER.info("self send: {}", ByteString.copyFromUtf8(pipe.getType()));
                 PipeHandleNotificationEvent event =
