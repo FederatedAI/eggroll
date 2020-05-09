bd7296b1491cc206120aa40ad683a4b364c9372d
hmoster <hwz_mjlwz@163.com>
2020-05-09 14:59:42 +0800

    get classpath with another way.

--- bin/eggroll_boot_standalone.py
+++ bin/eggroll_boot_standalone.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
         os.makedirs(os.path.join(eggroll_logs_dir, 'eggroll'))
 
     javahome = get_property(conf_file, "eggroll.resourcemanager.bootstrap.roll_pair_master.javahome")
-    classpath = get_property(conf_file, "eggroll.resourcemanager.bootstrap.roll_pair_master.classpath")
+    classpath = os.path.join(eggroll_home, 'conf/') + ";" + os.path.join(eggroll_home, 'lib/*')
     cluster_manager_port = get_property(conf_file, "eggroll.resourcemanager.clustermanager.port")
 
     if platform.system() == "Windows":
