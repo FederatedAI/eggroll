1b8a0c0969f5b587113ef1821e2c95428e4ce2b9
hmoster <dyt38HGX>
2020-05-21 20:16:50 +0800

    read the deploy mode from eggroll.properties, default cluster mode.

--- python/eggroll/roll_site/test/roll_site_test_asset.py
+++ python/eggroll/roll_site/test/roll_site_test_asset.py
@@ -56,10 +56,7 @@ def get_option(role, conf_file=default_props_file, deploy_mode=DeployModes.CLUST
         ErEndpoint(host=eggroll_configs[RollSiteConfKeys.EGGROLL_ROLLSITE_HOST.key],
                    port=int(eggroll_configs[RollSiteConfKeys.EGGROLL_ROLLSITE_PORT.key]))
 
-    if RollSiteConfKeys.EGGROLL_ROLLSITE_DEPLOY_MODE.key in eggroll_configs:
-        options['deploy_mode'] = eggroll_configs[RollSiteConfKeys.EGGROLL_ROLLSITE_DEPLOY_MODE.key]
-    else:
-        options['deploy_mode'] = deploy_mode
+    options[RollSiteConfKeys.EGGROLL_ROLLSITE_DEPLOY_MODE] = deploy_mode
 
     return options
 
