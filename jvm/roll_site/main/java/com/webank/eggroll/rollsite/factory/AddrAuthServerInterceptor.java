11ecd1095e0eb97677e56dce3bec4ccb95fc549f
hmoster <hwz_mjlwz@163.com>
2020-07-08 11:15:01 +0800

    add set_route_table and get_route_table

---
+++ jvm/roll_site/main/java/com/webank/eggroll/rollsite/factory/AddrAuthServerInterceptor.java
@@ -0,0 +1,24 @@
+package com.webank.eggroll.rollsite.factory;
+
+import io.grpc.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AddrAuthServerInterceptor implements ServerInterceptor {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final Context.Key<Object> REMOTE_ADDR
+            = Context.key("remoteAddr");
+
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        String inetSocketString = call.getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR).toString();
+
+        LOGGER.info("inetSocketString={},headers={},methodNameDesctiptor={}, methodName={}", inetSocketString,
+                headers, call.getMethodDescriptor(), call.getMethodDescriptor().getFullMethodName());
+
+        String remoteAddr = call.getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR).toString();
+        Context context = Context.current().withValue(REMOTE_ADDR, remoteAddr);
+        return Contexts.interceptCall(context, call, headers, next);
+    }
+}
