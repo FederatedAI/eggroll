6108f617a12285c55cf8d586c09ab5cb6a0b3558
hmoster <dyt38HGX>
2020-07-10 14:45:45 +0800

    add set_route_table and get_route_table

--- jvm/roll_site/main/java/com/webank/eggroll/rollsite/grpc/service/DataTransferPipedServerImpl.java
+++ jvm/roll_site/main/java/com/webank/eggroll/rollsite/grpc/service/DataTransferPipedServerImpl.java
@@ -426,7 +426,7 @@ public class DataTransferPipedServerImpl extends DataTransferServiceGrpc.DataTra
                 throw new IllegalArgumentException("srcIp cannot be null");
             } else {
                 String[] whiteList = proxyServerConf.getWhiteList();
-                if (Arrays.asList(whiteList).contains(srcIp)) {
+                if (whiteList != null && Arrays.asList(whiteList).contains(srcIp)) {
                     File jsonFile = new File(routeTablePath);
                     try {
                         String encoding = "UTF-8";
@@ -464,7 +464,7 @@ public class DataTransferPipedServerImpl extends DataTransferServiceGrpc.DataTra
                 throw new IllegalArgumentException("srcIp cannot be null");
             } else {
                 String[] whiteList = proxyServerConf.getWhiteList();
-                if (Arrays.asList(whiteList).contains(srcIp)) {
+                if (whiteList != null && Arrays.asList(whiteList).contains(srcIp)) {
                     String jsonString = request.getBody().getValue().toStringUtf8();
                     //String jsonString = format(result.toString());
                     try {
