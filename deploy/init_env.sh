37249ecc841c05fbff7925c45ce22b1f04e71a18
zengjice <jicezeng@gmail.com>
2020-07-15 16:42:58 +0800

    add init_env.sh

---
+++ deploy/init_env.sh
@@ -0,0 +1,21 @@
+#  Copyright (c) 2019 - now, Eggroll Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#	  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#
+export EGGROLL_HOME= 
+export PYTHONPATH=$EGGROLL_HOME/python 
+export EGGROLL_LOG_LEVEL=DEBUG
+export EGGROLL_RESOURCE_MANAGER_AUTO_BOOTSTRAP=0
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=
+source $EGGROLL_HOME/venv/bin/activate
