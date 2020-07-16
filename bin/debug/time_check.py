b9f8d6ffc3189061a087963682651348a5315159
maxwong <maxwong@live.cn>
2020-07-16 14:04:35 +0800

    1. Reindents with spaces.

--- bin/debug/time_check.py
+++ bin/debug/time_check.py
@@ -13,17 +13,17 @@
 #  limitations under the License.
 #
 #
-import os 
-import time 
-import argparse 
+import os
+import time
+import argparse
 
-arg_parser = argparse.ArgumentParser() 
-arg_parser.add_argument("-t","--time", type=int, help="Sleep time wait, default value 0s", default=0) 
-args = arg_parser.parse_args() 
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("-t","--time", type=int, help="Sleep time wait, default value 0s", default=0)
+args = arg_parser.parse_args()
 
-if args.time == 0: 
-	os.system('sh ./cluster_env_check.sh')
-else: 
-	while 1: 
-		os.system('sh ./cluster_env_check.sh')
-		time.sleep(args.time) 
+if args.time == 0:
+    os.system('sh ./cluster_env_check.sh')
+else:
+    while 1:
+        os.system('sh ./cluster_env_check.sh')
+        time.sleep(args.time)
