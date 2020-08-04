e0a6914b43720f0ab23a415930ad73cd6d9e254b
maxwong <maxwong@live.cn>
2020-08-04 10:27:18 +0800

    1. Runnable main.

---
+++ jvm/roll_site_2/main/scala/com/webank/eggroll/rollsite/RollSiteBootstrap.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2019 - now, Eggroll Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package com.webank.eggroll.rollsite
+
+import java.io.File
+
+import com.webank.eggroll.core.BootstrapBase
+import com.webank.eggroll.core.constant.{CoreConfKeys, RollSiteConfKeys}
+import com.webank.eggroll.core.session.StaticErConf
+import com.webank.eggroll.core.transfer.GrpcServerUtils
+import com.webank.eggroll.core.util.{CommandArgsUtils, Logging}
+
+class RollSiteBootstrap extends BootstrapBase with Logging {
+  private var port = 0
+  private var securePort = 0
+  private var confPath = ""
+
+  override def init(args: Array[String]): Unit = {
+    val cmd = CommandArgsUtils.parseArgs(args = args)
+    this.confPath = cmd.getOptionValue('c', "./conf/eggroll.properties")
+    StaticErConf.addProperties(confPath)
+    val confFile = new File(confPath)
+    StaticErConf.addProperty(CoreConfKeys.STATIC_CONF_PATH, confFile.getAbsolutePath)
+    logInfo(s"conf file: ${confFile.getAbsolutePath}")
+    this.port = cmd.getOptionValue('p', RollSiteConfKeys.EGGROLL_ROLLSITE_PORT.get()).toInt
+  }
+
+  override def start(): Unit = {
+    val plainServer = GrpcServerUtils.createServer(port = this.port, grpcServices = List(new DataTransferService))
+    plainServer.start()
+    this.port = plainServer.getPort
+
+    val msg = s"server started at ${port}"
+    logInfo(msg)
+    print(msg)
+  }
+}
