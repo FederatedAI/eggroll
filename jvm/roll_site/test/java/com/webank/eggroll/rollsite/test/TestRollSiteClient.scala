f052f22ac0f63bfe589c168264889150609c5567
maxwong <maxwong@live.cn>
2020-08-06 17:03:52 +0800

    1. Finished register jobid and session

--- jvm/roll_site/test/java/com/webank/eggroll/rollsite/test/TestRollSiteClient.scala
+++ jvm/roll_site/test/java/com/webank/eggroll/rollsite/test/TestRollSiteClient.scala
@@ -19,6 +19,7 @@
 package com.webank.eggroll.rollsite.test
 
 import com.webank.ai.eggroll.api.networking.proxy.Proxy
+import com.webank.ai.eggroll.api.networking.proxy.Proxy.{Model, Task}
 import com.webank.eggroll.core.meta.ErEndpoint
 import com.webank.eggroll.core.util.{Logging, ToStringUtils}
 import com.webank.eggroll.rollsite.DataTransferClient
@@ -31,15 +32,19 @@ class TestRollSiteClient extends Logging {
   private val bodyBuilder = Proxy.Data.newBuilder()
   private val packetBuilder = Proxy.Packet.newBuilder()
   private val topicBuilder = Proxy.Topic.newBuilder()
-  private val entryEndpoint = new ErEndpoint("localhost", 9370)
-  private val client = new DataTransferClient(entryEndpoint)
-  private val dstTopic = topicBuilder.setPartyId("10002").build()
+  private val endpoint10001 = new ErEndpoint("localhost", 9370)
+  private val client10001 = new DataTransferClient(endpoint10001)
+  private val topic10001 = topicBuilder.setPartyId("10001").build()
+
+  private val endpoint10002 = new ErEndpoint("localhost", 9470)
+  private val client10002 = new DataTransferClient(endpoint10001)
+  private val topic10002 = topicBuilder.setPartyId("10002").build()
 
   @Test
   def testUnaryCall(): Unit = {
 
-    val result = client.unaryCall(packetBuilder
-      .setHeader(headerBuilder.setSeq(12345).setAck(54321).setDst(dstTopic)).build())
+    val result = client10001.unaryCall(packetBuilder
+      .setHeader(headerBuilder.setSeq(12345).setAck(54321).setDst(topic10002)).build())
 
     logInfo(s"unary call response: ${ToStringUtils.toOneLineString(result)}")
   }
@@ -52,10 +57,19 @@ class TestRollSiteClient extends Logging {
     val ack = 20000
 
     for (i <- 0 until 2) {
-      data.append(packetBuilder.setHeader(headerBuilder.setSeq(seq + i).setAck(ack + i).setDst(dstTopic)).build())
+      data.append(packetBuilder.setHeader(headerBuilder.setSeq(seq + i).setAck(ack + i).setDst(topic10002)).build())
     }
 
-    val result = client.push(data.iterator)
+    val result = client10001.push(data.iterator)
     logInfo(s"push response: ${ToStringUtils.toOneLineString(result)}")
+  }
+
+  @Test
+  def testInitJobIdToSession(): Unit = {
+    val request = packetBuilder
+      .setHeader(headerBuilder.setSeq(12345).setDst(topic10001).setOperator("init_job_session_pair").setTask(Task.newBuilder().setModel(Model.newBuilder().setName("test_job").setDataKey("testing")))).build()
+
+    val response = client10001.unaryCall(request)
+    logInfo(s"init jobid to session result: ${ToStringUtils.toOneLineString(response)}")
   }
 }
