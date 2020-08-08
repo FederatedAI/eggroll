f1db7a673c33005ff239b98f983b4002e812a11e
maxwong <maxwong@live.cn>
2020-08-08 14:55:46 +0800

    1. Adds new put batch logic. 2.

--- jvm/roll_site/main/scala/com/webank/eggroll/rollsite/DataTransfer.scala
+++ jvm/roll_site/main/scala/com/webank/eggroll/rollsite/DataTransfer.scala
@@ -18,19 +18,16 @@
 
 package com.webank.eggroll.rollsite
 
-import java.nio.charset.StandardCharsets
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.{CountDownLatch, Future, ThreadPoolExecutor, TimeUnit}
 
 import com.google.common.cache.{CacheBuilder, CacheLoader, LoadingCache}
+import com.webank.ai.eggroll.api.networking.proxy.Proxy.Metadata
 import com.webank.ai.eggroll.api.networking.proxy.{DataTransferServiceGrpc, Proxy}
 import com.webank.eggroll.core.ErSession
 import com.webank.eggroll.core.constant.RollSiteConfKeys
-import com.webank.eggroll.core.datastructure.FifoBroker
-import com.webank.eggroll.core.meta.TransferModelPbMessageSerdes._
 import com.webank.eggroll.core.meta.{ErEndpoint, ErRollSiteHeader}
 import com.webank.eggroll.core.transfer.GrpcClientUtils
-import com.webank.eggroll.core.transfer.Transfer.RollSiteHeader
 import com.webank.eggroll.core.util.{ErrorUtils, Logging, ThreadPoolUtils, ToStringUtils}
 import io.grpc.stub.StreamObserver
 
@@ -41,93 +38,13 @@ class DataTransferService extends DataTransferServiceGrpc.DataTransferServiceImp
   /**
    */
   override def push(responseObserver: StreamObserver[Proxy.Metadata]): StreamObserver[Proxy.Packet] = {
-    // refer to ServerPushRequestStreamObserver, and simplifies it
-    new StreamObserver[Proxy.Packet] {
-      val inited = new AtomicBoolean(false)
-      val broker = new FifoBroker[Proxy.Packet]()
-      var processFunc: Proxy.Packet => Unit = _
-      var response: Proxy.Metadata = _
-      var client: DataTransferClient = _
-      var clientFuture: Future[Proxy.Metadata] = _
-      var isDst: Boolean = _
-      var rollSiteHeader: ErRollSiteHeader = _
-
-      override def onNext(request: Proxy.Packet): Unit = {
-        /**
-         * check if dst is myself:
-         *   - yes -> gets a ErSession and sends data to it using internal protocol (the one which TransferService is using), but reserve the possibility to use external protocol (proxy protocol).
-         *   - no -> forwards it to the next hop.
-         */
-
-        def whenDst(request: Proxy.Packet): Unit = {
-
-
-          // use internal protocol to put all
-          logInfo(s"got data in dst: ${ToStringUtils.toOneLineString(request)}")
-        }
-
-        def whenNotDst(request: Proxy.Packet): Unit = {
-          broker.broker.put(request)
-        }
-
-        val myPartyId = RollSiteConfKeys.EGGROLL_ROLLSITE_PARTY_ID.get()
-        val dstPartyId = request.getHeader.getDst.getPartyId
-        if (!inited.get()) {
-          // get host:port from router
-
-          processFunc = if (myPartyId.equals(dstPartyId)) {
-            isDst = true
-            logInfo("i am dst")
-            clientFuture = DataTransferService.dataTransferServerExecutor.submit(() => {
-              Proxy.Metadata.getDefaultInstance
-            })
-
-            rollSiteHeader = RollSiteHeader.parseFrom(
-              request.getHeader.getTask.getModel.getName.getBytes(StandardCharsets.ISO_8859_1)).fromProto()
-
-
-            whenDst
-          } else {
-            isDst = false
-            logInfo("i am not dst")
-
-            clientFuture = DataTransferService.dataTransferServerExecutor.submit(() => {
-              client = new DataTransferClient(ErEndpoint("localhost", 9470))
-              client.push(broker)
-            })
-            whenNotDst
-          }
-
-          response = request.getHeader
-          inited.compareAndSet(false, true)
-        }
-
-        processFunc(request)
-        logInfo(s"push on next: ${ToStringUtils.toOneLineString(request)}")
-      }
-
-      override def onError(throwable: Throwable): Unit = {
-        // error logic here
-        logError(throwable)
-      }
-
-      override def onCompleted(): Unit = {
-        // complete logic here
-        broker.signalWriteFinish()
-
-        response = clientFuture.get()
-
-        responseObserver.onNext(response)
-        responseObserver.onCompleted()
-        logInfo("completed")
-      }
-    }
+    new ProxyForwardStreamObserver(responseObserver)
   }
 
   /**
    */
   override def unaryCall(request: Proxy.Packet,
-                         responseObserver: StreamObserver[Proxy.Packet]): Unit = {
+                         responseSO: StreamObserver[Proxy.Packet]): Unit = {
     /**
      * Check if dst is myself.
      *   - yes -> check command to see what the request wants.
@@ -139,19 +56,21 @@ class DataTransferService extends DataTransferServiceGrpc.DataTransferServiceImp
       val myPartyId = RollSiteConfKeys.EGGROLL_ROLLSITE_PARTY_ID.get()
       val dstPartyId = request.getHeader.getDst.getPartyId
       val result = if (myPartyId.equals(dstPartyId)) {
-        processCommand(request)
+        logInfo(s"unary call: ${ToStringUtils.toOneLineString(request)}")
+        request
+        //processCommand(request)
       } else {
-        val client = new DataTransferClient(ErEndpoint("localhost", 9470))
+        val client = new DataTransferForwarder(ErEndpoint("localhost", 9470))
         client.unaryCall(request)
       }
 
-      responseObserver.onNext(result)
-      responseObserver.onCompleted()
+      responseSO.onNext(result)
+      responseSO.onCompleted()
     } catch {
       case t: Throwable =>
         val wrapped = ErrorUtils.toGrpcRuntimeException(t)
         logError(wrapped)
-        responseObserver.onError(wrapped)
+        responseSO.onError(t)
     }
   }
 
@@ -203,62 +122,137 @@ object DataTransferService {
     })
 }
 
-class DataTransferClient(defaultEndpoint: ErEndpoint, isSecure: Boolean = false) extends Logging {
 
-  def push(requests: Iterator[Proxy.Packet],
+////////////////////////////
+
+
+class DataTransferForwarder(defaultEndpoint: ErEndpoint,
+                            isSecure: Boolean = false) extends Logging {
+  var stub:DataTransferServiceGrpc.DataTransferServiceStub = _
+  def push(requests: StreamObserver[Proxy.Metadata],
            endpoint: ErEndpoint = defaultEndpoint,
-           options: Map[String, String] = Map.empty): Proxy.Metadata = {
+           options: Map[String, String] = Map.empty): StreamObserver[Proxy.Packet] = {
     val channel = GrpcClientUtils.getChannel(endpoint, isSecure, options)
-    val stub = DataTransferServiceGrpc.newStub(channel)
-    var result: Proxy.Metadata = null
-    val finishLatch = new CountDownLatch(1)
+    DataTransferServiceGrpc.newStub(channel)
 
-    val streamObserver = stub.push(new StreamObserver[Proxy.Metadata] {
-      // define what to do when server responds
-      override def onNext(v: Proxy.Metadata): Unit = {
-        result = v
-        logInfo(s"response metadata: ${result}")
-      }
-
-      // define what to do when server gives error - backward propagation
-      override def onError(throwable: Throwable): Unit = {
-        // process error here
-        finishLatch.countDown()
-      }
-
-      // define what to do when server finishes
-      override def onCompleted(): Unit = {
-        // process finish here
-        finishLatch.countDown()
-      }
-    })
-
-    for (request <- requests) {
-      streamObserver.onNext(request)
-    }
-
-    streamObserver.onCompleted()
-    finishLatch.await()
-
-    result
+    stub.push(requests)
   }
 
-  def pushAsync(requests: Iterator[Proxy.Packet],
-                endpoint: ErEndpoint = defaultEndpoint,
-                options: Map[String, String] = Map.empty): Future[Proxy.Metadata] = {
-    DataTransferService.dataTransferClientExecutor.submit(() => {
-      push(requests, endpoint, options)
-    })
-  }
-
-  def unaryCall(request: Proxy.Packet,
+  def unaryCall(packet: Proxy.Packet,
                 endpoint: ErEndpoint = defaultEndpoint,
                 options: Map[String, String] = Map.empty): Proxy.Packet = {
     val channel = GrpcClientUtils.getChannel(endpoint, isSecure, options)
     val stub = DataTransferServiceGrpc.newBlockingStub(channel)
 
-    stub.unaryCall(request)
+    stub.unaryCall(packet)
   }
-
 }
 
+
+class ProxyForwardStreamObserver(responseSO: StreamObserver[Proxy.Metadata]) extends StreamObserver[Proxy.Packet] with Logging {
+  private var forwarderSO: StreamObserver[Proxy.Packet] = _
+  private var inited = new AtomicBoolean(false)
+
+  override def onNext(request: Proxy.Packet): Unit = {
+    if (!inited.get()) {
+      val myPartyId = RollSiteConfKeys.EGGROLL_ROLLSITE_PARTY_ID.get()
+      val dstPartyId = request.getHeader.getDst.getPartyId
+
+      forwarderSO = if (myPartyId.equals(dstPartyId)) {
+        new SinkRequestStreamObserver(responseSO)
+      } else {
+        new ForwardRequestStreamObserver(responseSO)
+      }
+      inited.compareAndSet(false, true)
+    }
+
+    forwarderSO.onNext(request)
+  }
+
+  override def onError(throwable: Throwable): Unit = {
+    forwarderSO.onError(throwable)
+  }
+
+  override def onCompleted(): Unit = {
+
+    forwarderSO.onCompleted()
+  }
+}
+
+
+class SinkRequestStreamObserver(responseSO: StreamObserver[Proxy.Metadata])
+  extends StreamObserver[Proxy.Packet] with Logging {
+  private var response: Proxy.Metadata = _
+  private var rollSiteHeader: ErRollSiteHeader = _
+
+  override def onNext(request: Proxy.Packet): Unit = {
+    logInfo(s"sink on next: ${ToStringUtils.toOneLineString(request)}")
+  }
+
+  override def onError(throwable: Throwable): Unit = {
+    // error logic here
+    logError(throwable)
+  }
+
+  override def onCompleted(): Unit = {
+    // complete logic here
+    responseSO.onNext(Proxy.Metadata.getDefaultInstance)
+    responseSO.onCompleted()
+
+    logInfo("completed")
+    Thread.sleep(1000)
+  }
+}
+
+
+class ForwardRequestStreamObserver(prevRespSO: StreamObserver[Proxy.Metadata])
+  extends StreamObserver[Proxy.Packet] with Logging {
+  private val inited = new AtomicBoolean(false)
+  private var nextReqSO: StreamObserver[Proxy.Packet] = _
+  private val self = this
+  private val finishLatch = new CountDownLatch(1)
+
+  val forwarder = new DataTransferForwarder(new ErEndpoint("localhost", 9470))
+  override def onNext(request: Proxy.Packet): Unit = {
+    logInfo(s"onnext: ${ToStringUtils.toOneLineString(request)}")
+
+    if (!inited.get()) {
+
+
+      nextReqSO = forwarder.push(new StreamObserver[Proxy.Metadata] {
+
+        override def onNext(value: Proxy.Metadata): Unit = {
+
+          logInfo("response received")
+        }
+
+        override def onError(t: Throwable): Unit = {
+          //self.onError(t)
+        }
+
+        override def onCompleted(): Unit = {
+          logInfo("prevRespSO onComplete")
+          prevRespSO.onNext(Proxy.Metadata.getDefaultInstance)
+          finishLatch.countDown()
+          prevRespSO.onCompleted()
+
+          logInfo("finish forwarding")
+        }
+      })
+    }
+
+    nextReqSO.onNext(request)
+    logInfo("forwarding")
+  }
+
+  override def onError(throwable: Throwable): Unit = {
+    //nextSO.onError(throwable)
+    //prevSO.onError(throwable)
+  }
+
+  override def onCompleted(): Unit = {
+    logInfo("nextReqSO onComplete")
+    nextReqSO.onCompleted()
+
+  }
+}
