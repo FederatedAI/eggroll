6aa100f361d072a097f6e95fec7190a2432defdd
shiqili <shiqili@webank.com>
2020-08-21 15:36:38 +0800

    route table query add default permission control.

--- jvm/roll_site/main/scala/com/webank/eggroll/rollsite/Router.scala
+++ jvm/roll_site/main/scala/com/webank/eggroll/rollsite/Router.scala
@@ -7,11 +7,20 @@ import org.json.{JSONArray, JSONObject}
 
 object Router {
   private var routerTable: JSONObject = _
+  private var defaultEnable: Boolean = true
 
   def initOrUpdateRouterTable(path: String): Unit = {
     val source = Source.fromFile(path,"UTF-8")
     val str = source.mkString
-    routerTable = new JSONObject(str).get("route_table").asInstanceOf[JSONObject]
+    val js = new JSONObject(str)
+    routerTable = js.get("route_table").asInstanceOf[JSONObject]
+    try {
+      defaultEnable = js.get("permission").asInstanceOf[JSONObject]
+        .get("default_allow").asInstanceOf[Boolean]
+    } catch {
+      case _: Throwable => defaultEnable = true
+    }
+
   }
 
   def query(partyId: String, role: String = "default"): ErEndpoint = {
@@ -19,18 +28,32 @@ object Router {
       throw new Exception("The routing table is not initialized!")
     }
 
-    if (!routerTable.has(partyId)) {
-      throw new Exception(s"The routing table not have current party:${partyId}")
+    if (!routerTable.has(partyId) && !routerTable.has("default")) {
+      throw new Exception(s"The routing table not have current party:${partyId} and default party.")
     }
 
-    val rt = routerTable.get(partyId).asInstanceOf[JSONObject]
+    val curParty = if (routerTable.has(partyId)) {partyId} else {
+      if (defaultEnable) {
+        "default"
+      } else {
+        throw new Exception(s"The routing table not have current party:${partyId} and disable default party.")
+      }
+
+    }
+    val rt = routerTable.get(curParty).asInstanceOf[JSONObject]
 
     if (!rt.has(role) && !rt.has("default")) {
-      throw new Exception(s"The routing table not have current party:${partyId}")
+      throw new Exception(s"The routing table not have current role:${role}")
     }
 
-    val curRole = if (rt.has(role)) {role} else {"default"}
-    val default: JSONObject = routerTable.get(partyId).asInstanceOf[JSONObject]
+    val curRole = if (rt.has(role)) {role} else {
+      if (defaultEnable) {
+        "default"
+      } else {
+        throw new Exception(s"The routing table not have current role:${role} and disable default role.")
+      }
+    }
+    val default: JSONObject = routerTable.get(curParty).asInstanceOf[JSONObject]
       .get(curRole).asInstanceOf[JSONArray]
       .get(0).asInstanceOf[JSONObject]
     val host = default.get("ip").asInstanceOf[String]
