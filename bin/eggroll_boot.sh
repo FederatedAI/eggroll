b3f0017ef9e8ddf5e283d15ce4b9ae902ddbc4ab
maxwong <maxwong@live.cn>
2020-09-16 19:47:23 +0800

    1. Updates start script.

--- bin/eggroll_boot.sh
+++ bin/eggroll_boot.sh
@@ -8,24 +8,28 @@ SCRIPT_NAME=$(basename $0)
 SHELL_FOLDER=$(dirname "$0")
 BASH=`which bash`
 
+echo "==================="
+echo "start_time=`date +'%Y-%m-%d %H:%M:%S.%N'`"
 echo "$0, pid: $$"
 echo "script params: $@ "
 
+echo
 
 echo "------ mkdir starts ------"
 mkdir -p $SHELL_FOLDER/pid
 #pid_file=$SHELL_FOLDER/pid/$pname.pid
 echo "------ mkdir ends ------"
 
+echo
 
 echo "------ env starts -----"
 env
 echo "------ env ends -----"
 
+echo
 
 echo "------ script body ------"
-echo
-echo
+
 # TODO:2: check pid file and delete?
 if [[ $sub_cmd == "start" ]]; then
   ${BASH} ${exe} &
@@ -38,12 +42,17 @@ elif [[ $sub_cmd == "stop" ]]; then
   #echo "stop: $cmd, pid $pid"
   echo "stop: $cmd"
   eval ${cmd}
-  echo "pid=$$, ppid=$PPID, eval pid=$!, stop result=$? (0 -> succeed, 1 -> failed)"
+  echo "pid=$$, ppid=$PPID, eval pid=$!, stop result=$? (0 -> successful, 1 -> failed)"
 elif [[ $sub_cmd == "kill" ]]; then
   #pid=`cat $pid_file`
   cmd="${exe} | grep -v ${SCRIPT_NAME} | awk '{print \$2}' | xargs kill -9"
   #echo "kill: $cmd, pid $pid"
   echo "kill: $cmd"
   eval ${cmd}
-  echo "pid=$$, ppid=$PPID, eval pid=$!, kill result=$? (0 -> succeed, 1 -> failed)"
+  echo "pid=$$, ppid=$PPID, eval pid=$!, kill result=$? (0 -> successful, 1 -> failed)"
 fi
+
+echo "==================="
+echo "end_time=`date +'%Y-%m-%d %H:%M:%S.%N'`"
+echo
+echo
