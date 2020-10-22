3fdff414e28e406bb04023cd4183052d79e89647
maxwong <maxwong@live.cn>
2020-10-22 15:52:27 +0800

    1. Modifies default value of no data op.

--- jvm/core/main/scala/com/webank/eggroll/core/constant/ConfKeys.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/constant/ConfKeys.scala
@@ -152,7 +152,7 @@ object RollSiteConfKeys {
   val EGGROLL_ROLLSITE_POLLING_Q_POLL_INTERVAL_SEC = ErConfKey("eggroll.rollsite.polling.q.poll.interval.sec", 60)
   val EGGROLL_ROLLSITE_POLLING_Q_OFFER_INTERVAL_SEC = ErConfKey("eggroll.rollsite.polling.q.offer.interval.sec", 60)
   val EGGROLL_ROLLSITE_POLLING_NO_DATA_TIMEOUT_SEC = ErConfKey("eggroll.rollsite.polling.no.data.timeout.sec", 300)
-  val EGGROLL_ROLLSITE_POLLING_EXCHANGER_DATA_OP_TIMEOUT_SEC = ErConfKey("eggroll.rollsite.polling.exchanger.data.op.timeout.sec", 360)
+  val EGGROLL_ROLLSITE_POLLING_EXCHANGER_DATA_OP_TIMEOUT_SEC = ErConfKey("eggroll.rollsite.polling.exchanger.data.op.timeout.sec", 240) // should always be < no data timeout and python per stream deadline
   val EGGROLL_ROLLSITE_COORDINATOR = ErConfKey("eggroll.rollsite.coordinator")
   val EGGROLL_ROLLSITE_HOST = ErConfKey("eggroll.rollsite.host", "127.0.0.1")
   val EGGROLL_ROLLSITE_PORT = ErConfKey("eggroll.rollsite.port", "9370")
