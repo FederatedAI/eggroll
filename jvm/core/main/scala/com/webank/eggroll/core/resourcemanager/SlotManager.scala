876dbfec66ca884fbcde12b3e21116b5867d30fd
forgive_dengkai <forgive_dengkai@163.com>
2023-04-23 16:38:44 +0800

    add resource manager
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>

--- jvm/core/main/scala/com/webank/eggroll/core/resourcemanager/SlotManager.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/resourcemanager/SlotManager.scala
@@ -3,17 +3,21 @@ package com.webank.eggroll.core.resourcemanager
 import com.webank.eggroll.core.constant.ResourceTypes
 import com.webank.eggroll.core.meta.ErResource
 import com.webank.eggroll.core.util.Logging
+import org.apache.commons.lang3.StringUtils
 
 import java.util.concurrent.atomic.AtomicLong
 import scala.collection.mutable
 
 object SlotManager extends Logging{
 
-   var allocatedSlots = mutable.Map[String,Slot]()
+   var allocatedSlots = mutable.Map[Long,Slot]()
+
+   var sessionSlot = mutable.Map[String,mutable.Set[Long]]()
 
    var currentSlotId = new  AtomicLong(0)
 
    def   allocateSlot( sessionId:String,containerId: String,erResource:Array[ErResource]): Slot =synchronized {
+      require(StringUtils.isNotEmpty(sessionId))
       var prepareToAllocateResource = mutable.Map[String,Long]();
       erResource.groupBy(n=>{
          n.resourceType
@@ -30,29 +34,31 @@ object SlotManager extends Logging{
          var alreadyAllocated = NodeResourceManager.allocateResource(e.resourceType,e.total)
          logInfo(s"sessionId ${sessionId} containerId ${containerId} allocate ${e.resourceType} ${e.total} , already allocate ${alreadyAllocated}")
       })
-      Slot(currentSlotId.addAndGet(1),erResource);
+      var slot = Slot(slotId=currentSlotId.addAndGet(1),
+         sessionId = sessionId,
+         containerId = containerId,
+         resources=erResource);
+      allocatedSlots.put(slot.slotId,slot)
+      if(sessionSlot.contains(sessionId)){
+         sessionSlot.get(sessionId).get.+=(slot.slotId)
+      }
+      slot
    }
 
-//   private def checkResourceIsEnough(rtype:String ,count :Long ): Boolean ={
-//      var resourceWrapper =  NodeResourceManager.getResourceWrapper(rtype)
-//      if(resourceWrapper!=None){
-//         var  left = resourceWrapper.get.total.get() - resourceWrapper.get.allocated.get()
-//         if(count>= left)
-//            return true;
-//      }
-//      false
-//   }
-//   private  def  allocateResource(rtype:String ,count:Long):Long ={
-//      require(count>0)
-//      var resourceWrapper =  NodeResourceManager.getResourceWrapper(rtype)
-//      resourceWrapper.get.allocated.addAndGet(count);
-//   }
+   def  freeSlotBySlotId(slotId:Long):Unit = synchronized{
+      if(allocatedSlots.contains(slotId)){
+         allocatedSlots.get(slotId).get.resources.foreach(r=>{
+            NodeResourceManager.freeResource(r.resourceType,r.total)
+         })
+      }
+   }
 
-
-
-   def  freeSlot(slotId:String):Boolean = synchronized{
-
-     false
+   def  freeSlotBySessionId(sessionId: String ): Unit={
+      if(sessionSlot.contains(sessionId)){
+         sessionSlot.get(sessionId).get.foreach(slotId=>{
+            freeSlotBySlotId(slotId)
+         })
+      }
    }
 
 
@@ -66,6 +72,8 @@ object SlotManager extends Logging{
 
 }
 case class Slot(slotId:Long  ,
+                sessionId: String ,
+                containerId: String,
                 resources:Array[ErResource]=Array())
 
 
