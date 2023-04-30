d852044e5bece602af62d7252353794ed8880b72
forgive_dengkai <forgive_dengkai@163.com>
2023-04-30 22:37:12 +0800

    resource
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>

---
+++ jvm/core/test/scala/com/webank/eggroll/core/resourcemanager/TestSessionManager.scala
@@ -0,0 +1,22 @@
+package com.webank.eggroll.core.resourcemanager
+
+import com.webank.eggroll.core.client.ClusterManagerClient
+import org.junit.Test
+
+class TestSessionManager {
+
+  @Test
+  def testGetOrCreate():Unit = {
+
+    val numbers = List(1,2,3,4).map(n=>(n,""))(collection.breakOut)
+    val clusterManagerClient = new ClusterManagerClient("localhost",4670)
+    val result = clusterManagerClient.getOrCreateSession(sessionMeta = TestAssets.getOrCreateSessionMeta)
+    println(result)
+  }
+
+  @Test
+  def testStop(): Unit = {
+    val clusterManagerClient = new ClusterManagerClient()
+    val result = clusterManagerClient.stopSession(sessionMeta = TestAssets.getOrCreateSessionMeta)
+  }
+}
