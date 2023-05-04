3f985fa4ac54a371a02833c1a73405277ea0afdb
forgive_dengkai <forgive_dengkai@163.com>
2023-05-04 11:06:56 +0800

    resource
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- jvm/core/main/scala/com/webank/eggroll/core/resourcemanager/job/ClusterManagerJobService.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/resourcemanager/job/ClusterManagerJobService.scala
@@ -2,6 +2,7 @@ package com.webank.eggroll.core.resourcemanager.job
 
 import com.webank.eggroll.core.client.NodeManagerClient
 import com.webank.eggroll.core.constant._
+import com.webank.eggroll.core.error.ErSessionException
 import com.webank.eggroll.core.meta._
 import com.webank.eggroll.core.resourcemanager.{ClusterResourceManager, SessionMetaDao}
 import com.webank.eggroll.core.resourcemanager.metadata.ServerNodeCrudOperator
@@ -9,7 +10,9 @@ import com.webank.eggroll.core.util.Logging
 
 import scala.collection.JavaConverters.mapAsJavaMapConverter
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 import scala.util.Random
+import scala.util.control.Breaks.{break, breakable}
 
 class ClusterManagerJobService extends Logging {
   private val smDao = new SessionMetaDao
@@ -75,11 +78,52 @@ class ClusterManagerJobService extends Logging {
 
         // start containers
         dispatchedProcessors.groupBy(_._2).par.foreach { case (node, nodeAndProcessors) =>
-          val processors = nodeAndProcessors.map(_._1)
+          val processors = nodeAndProcessors.map(_._1.copy(sessionId = submitJobMeta.id))
           val nodeManagerClient = new NodeManagerClient(node.endpoint)
           ClusterResourceManager.preAllocateResource(processors)
           nodeManagerClient.startJobContainers(submitJobMeta.copy(processors = processors))
         }
+
+        val startTimeout = System.currentTimeMillis() + SessionConfKeys.EGGROLL_SESSION_START_TIMEOUT_MS.get().toLong
+        var isStarted = false
+        breakable {
+          while (System.currentTimeMillis() <= startTimeout) {
+            val cur =     smDao.getSessionMain(submitJobMeta.id)
+            if (cur.activeProcCount < submitJobMeta.worldSize) {
+              Thread.sleep(100)
+            } else {
+              isStarted = true
+              break
+            }
+          }
+        }
+
+        if (!isStarted) {
+          val curDetails = smDao.getSession(submitJobMeta.id)
+          // last chance to check
+          if (curDetails.activeProcCount < submitJobMeta.worldSize) {
+            dispatchedProcessors.groupBy(_._2).par.foreach { case (node, nodeAndProcessors) =>
+              val processors = nodeAndProcessors.map(_._1.copy(sessionId = submitJobMeta.id))
+              val nodeManagerClient = new NodeManagerClient(node.endpoint)
+              ClusterResourceManager.preAllocateResource(processors)
+              nodeManagerClient.killJobContainers(submitJobMeta.copy(processors = processors))
+            }
+
+            val builder = new mutable.StringBuilder()
+            builder.append(s"unable to start all processors for session id: '${submitJobMeta.id}'. ")
+              .append(s"Please check corresponding bootstrap logs at '${CoreConfKeys.EGGROLL_LOGS_DIR.get()}/${submitJobMeta.id}' to check the reasons. Details:\n")
+              .append("=================\n")
+              .append(s"total processors: ${curDetails.totalProcCount}, \n")
+              .append(s"started count: ${curDetails.activeProcCount}, \n")
+              .append(s"not started count: ${curDetails.totalProcCount - curDetails.activeProcCount}, \n")
+//              .append(s"current active processors per node: ${activesPerNode}, \n")
+//              .append(s"not started processors and their nodes: ${inactivesToNode}")
+            val exception = new ErSessionException(builder.toString())
+            throw exception
+          }
+        }
+
+
 
         // FIXME: update?
         smDao.updateSessionMain(ErSessionMeta(
