9bb7e075108d6100e25008dafed4142fe953191f
sagewe <wbwmat@gmail.com>
2023-05-10 14:49:26 +0800

    fix store scripts
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- jvm/core/main/scala/com/webank/eggroll/core/resourcemanager/job/container/DeepSpeedContainer.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/resourcemanager/job/container/DeepSpeedContainer.scala
@@ -86,59 +86,14 @@ object DeepSpeedRunPy {
             storePort: Int,
             worldSize: Int,
             rank: Int,
-            backend: String): Array[Byte] =
+            backend: String): Array[Byte] = {
+
     f"""
-       |import os
-       |import runpy
-       |import sys
-       |from datetime import timedelta
+       |def main(session_id, script_path, eggroll_home, store_host, store_port, world_size, rank, backend):
+       |    from eggroll.deepspeed.store import EggrollStore
+       |    from torch import distributed
        |
-       |from torch import distributed
-       |from torch.distributed import Store
-       |
-       |
-       |def main(session_id, script_path, eggroll_home, store_host, store_port, world_size, rank, backend="nccl"):
-       |    sys.path.append(f"{eggroll_home}/python")
-       |    os.environ["EGGROLL_HOME"] = eggroll_home
-       |
-       |    from eggroll.core.client import ClusterManagerClient
-       |    from eggroll.core.deepspeed.store_model import RendezvousStoreSetRequest, RendezvousStoreGetRequest, RendezvousStoreAddRequest
-       |
-       |    class EggrollStore(Store):
-       |        def __init__(self, host, port, prefix, timeout: timedelta = timedelta(seconds=300)):
-       |            self._host = host
-       |            self._port = port
-       |            self._prefix = prefix
-       |            self._timeout = timeout
-       |            super().__init__()
-       |
-       |        def get(self, key, timeout: timedelta = None):
-       |            if isinstance(key, str):
-       |                key = key.encode()
-       |            if timeout is None:
-       |                timeout = self._timeout
-       |            cluster_manager_client = ClusterManagerClient(options={})
-       |            get_request = RendezvousStoreGetRequest(prefix=self._prefix, key=key, timeout=timeout)
-       |            response = cluster_manager_client.rendezvous_store_get(get_request)
-       |            return response.value
-       |
-       |        def set(self, key, value):
-       |            if isinstance(key, str):
-       |                key = key.encode()
-       |            cluster_manager_client = ClusterManagerClient(options={})
-       |            set_request = RendezvousStoreSetRequest(prefix=self._prefix, key=key, value=value)
-       |            return cluster_manager_client.rendezvous_store_set(set_request)
-       |
-       |        def add(self, key, amount):
-       |            if isinstance(key, str):
-       |                key = key.encode()
-       |            cluster_manager_client = ClusterManagerClient(options={})
-       |            add_request = RendezvousStoreAddRequest(prefix=self._prefix, key=key, amount=amount)
-       |            response = cluster_manager_client.rendezvous_store_add(add_request)
-       |            return response.amount
-       |
-       |
-       |    store = EggrollStore(store_host, store_port, "session_id")
+       |    store = EggrollStore(store_host, store_port, session_id)
        |    distributed.init_process_group(backend=backend, store=store, world_size=world_size, rank=rank)
        |    runpy.run_path(script_path, run_name='__main__')
        |
@@ -156,6 +111,7 @@ object DeepSpeedRunPy {
        |    )
        |
        |""".stripMargin.getBytes
+  }
 
   val runPyName: String = "_run.py"
 }
