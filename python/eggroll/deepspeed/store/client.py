59a9197f8582de4a2878586d23701902ca5c3fb6
sagewe <wbwmat@gmail.com>
2023-05-10 13:18:55 +0800

    refact python side codes
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- python/eggroll/deepspeed/store/client.py
+++ python/eggroll/deepspeed/store/client.py
@@ -1,76 +1,46 @@
 from datetime import timedelta
-
-from google.protobuf.duration_pb2 import Duration
 from torch.distributed import Store
-
-from eggroll.config import Config
-from eggroll.core.command.command_uri import RendezvousStoreCommands
-from eggroll.core.proto import deepspeed_pb2
 from ..client import BaseClient
+from .commands import RendezvousStoreCommands
+from .model import RendezvousStoreSetRequest, RendezvousStoreSetResponse, RendezvousStoreGetRequest, \
+    RendezvousStoreGetResponse, RendezvousStoreAddRequest, RendezvousStoreAddResponse
 
 
 class EggrollStore(Store):
-    def __init__(
-        self,
-        config: Config,
-        host,
-        port,
-        prefix,
-        timeout: timedelta = timedelta(hours=24),
-    ):
-        self._config = config
+    def __init__(self, host, port, prefix, timeout: timedelta = timedelta(seconds=300)):
+        self._host = host
+        self._port = port
         self._prefix = prefix
         self._timeout = timeout
         super().__init__()
-
-        self._client = BaseClient(config=config, host=host, port=port)
 
     def get(self, key, timeout: timedelta = None):
         if isinstance(key, str):
             key = key.encode()
         if timeout is None:
             timeout = self._timeout
-        seconds = int(timeout.total_seconds())
-        nanos = int((timeout - timedelta(seconds=seconds)).microseconds * 1000)
-        response = self._client.do_sync_request(
-            input=deepspeed_pb2.StoreGetRequest(
-                prefix=self._prefix,
-                key=key,
-                timeout=Duration(seconds=seconds, nanos=nanos),
-            ),
-            output_type=deepspeed_pb2.StoreGetResponse,
-            command_uri=RendezvousStoreCommands.GET,
-        )
-        if response.is_timeout:
-            raise RuntimeError("Socket Timeout")
+        deepspeed_client = BaseClient()
+        get_request = RendezvousStoreGetRequest(prefix=self._prefix, key=key, timeout=timeout)
+        response = deepspeed_client.do_sync_request_internal(get_request,
+                                                             output_type=RendezvousStoreGetResponse,
+                                                             command_uri=RendezvousStoreCommands.GET)
         return response.value
 
     def set(self, key, value):
         if isinstance(key, str):
             key = key.encode()
-        return self._client.do_sync_request(
-            deepspeed_pb2.StoreSetRequest(prefix=self._prefix, key=key, value=value),
-            output_type=deepspeed_pb2.StoreSetResponse,
-            command_uri=RendezvousStoreCommands.SET,
-        )
+        deepspeed_client = BaseClient()
+        set_request = RendezvousStoreSetRequest(prefix=self._prefix, key=key, value=value)
+        return deepspeed_client.do_sync_request_internal(set_request,
+                                                         output_type=RendezvousStoreSetResponse,
+                                                         command_uri=RendezvousStoreCommands.SET)
 
     def add(self, key, amount):
         if isinstance(key, str):
             key = key.encode()
-        response = self._client.do_sync_request(
-            deepspeed_pb2.StoreAddRequest(prefix=self._prefix, key=key, amount=amount),
-            output_type=deepspeed_pb2.StoreAddResponse,
-            command_uri=RendezvousStoreCommands.ADD,
-        )
+        deepspeed_client = BaseClient()
+        add_request = RendezvousStoreAddRequest(prefix=self._prefix, key=key, amount=amount)
+        response = deepspeed_client.do_sync_request_internal(add_request,
+                                                             output_type=RendezvousStoreAddResponse,
+                                                             command_uri=RendezvousStoreCommands.ADD)
         return response.amount
-
-    def destroy(self):
-        return destroy(self._client, self._prefix)
-
-
-def destroy(client, prefix):
-    return client.do_sync_request(
-        deepspeed_pb2.StoreDestroyRequest(prefix=prefix),
-        output_type=deepspeed_pb2.StoreDestroyResponse,
-        command_uri=RendezvousStoreCommands.DESTROY,
-    )
