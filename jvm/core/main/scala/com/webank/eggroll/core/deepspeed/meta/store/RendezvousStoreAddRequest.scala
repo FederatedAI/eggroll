5ee055ad7cc12e6a0451f7c8b60f29d0f0fdee82
sagewe <wbwmat@gmail.com>
2023-05-12 13:17:10 +0800

    refact deepspeed and container
    Signed-off-by: sagewe <wbwmat@gmail.com>

---
+++ jvm/core/main/scala/com/webank/eggroll/core/deepspeed/meta/store/RendezvousStoreAddRequest.scala
@@ -0,0 +1,27 @@
+package com.webank.eggroll.core.deepspeed.meta.store
+
+import com.google.protobuf.ByteString
+import com.webank.eggroll.core.meta.Deepspeed
+
+import scala.language.implicitConversions
+
+case class RendezvousStoreAddRequest(prefix: String, key: K, amount: Long)
+
+object RendezvousStoreAddRequest {
+  implicit def serialize(src: RendezvousStoreAddRequest): Array[Byte] = {
+    val builder = Deepspeed.StoreAddRequest.newBuilder()
+      .setPrefix(src.prefix)
+      .setKey(ByteString.copyFrom(src.key.toArray))
+      .setAmount(src.amount)
+    builder.build().toByteArray
+  }
+
+  implicit def deserialize(byteString: ByteString): RendezvousStoreAddRequest = {
+    val src = Deepspeed.StoreAddRequest.parseFrom(byteString)
+    RendezvousStoreAddRequest(
+      prefix = src.getPrefix,
+      key = src.getKey.toByteArray.toVector,
+      amount = src.getAmount
+    )
+  }
+}
