70522debbd5e18b2ebe0fcbd47c6f1327981fcf2
sagewe <wbwmat@gmail.com>
2023-05-19 23:25:18 +0800

    auto session_id and format code
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- python/eggroll/deepspeed/client.py
+++ python/eggroll/deepspeed/client.py
@@ -1,17 +1,15 @@
 import logging
-import typing
 import time
+import typing
 
-from eggroll.core.command.command_model import ErCommandRequest, \
-    ErCommandResponse
+from eggroll.core.command.command_model import ErCommandRequest, ErCommandResponse
 from eggroll.core.conf_keys import ClusterManagerConfKeys
 from eggroll.core.grpc.factory import GrpcChannelFactory
 from eggroll.core.meta_model import ErEndpoint
 from eggroll.core.proto import command_pb2_grpc
-from eggroll.core.utils import get_static_er_conf
-from eggroll.core.utils import time_now_ns
+from eggroll.core.utils import get_static_er_conf, time_now_ns
 
-T = typing.TypeVar('T')
+T = typing.TypeVar("T")
 
 L = logging.getLogger(__name__)
 
@@ -26,15 +24,14 @@ class BaseClient:
                 port = static_er_conf.get(ClusterManagerConfKeys.CONFKEY_CLUSTER_MANAGER_PORT, None)
         if not host or not port:
             raise ValueError(
-                f'failed to load host or port in creating cluster manager client. host: {host}, port: {port}')
+                f"failed to load host or port in creating cluster manager client. host: {host}, port: {port}"
+            )
 
         self._endpoint = ErEndpoint(host, int(port))
         self._channel_factory = GrpcChannelFactory()
 
     def do_sync_request(self, input, output_type: typing.Type[T], command_uri) -> T:
-        request = ErCommandRequest(id=time_now_ns(),
-                                   uri=command_uri._uri,
-                                   args=[input.SerializeToString()])
+        request = ErCommandRequest(id=time_now_ns(), uri=command_uri._uri, args=[input.SerializeToString()])
         try:
             start = time.time()
             L.debug(f"[CC] calling: {self._endpoint} {command_uri} {request}")
@@ -52,5 +49,5 @@ class BaseClient:
             else:
                 return None
         except Exception as e:
-            L.exception(f'Error calling to {self._endpoint}, command_uri: {command_uri}, req:{request}')
+            L.exception(f"Error calling to {self._endpoint}, command_uri: {command_uri}, req:{request}")
             raise Exception(f"failed to call {command_uri} to {self._endpoint}: {e}")
