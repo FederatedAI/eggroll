0535a26273e259076544981ad95fe7d75e019341
forgive_dengkai <forgive_dengkai@163.com>
2023-05-20 10:51:58 +0800

    add resource logic
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- jvm/core/test/scala/com/webank/eggroll/core/resourcemanager/TestAssets.scala
+++ jvm/core/test/scala/com/webank/eggroll/core/resourcemanager/TestAssets.scala
@@ -3,6 +3,7 @@ package com.webank.eggroll.core.resourcemanager
 import com.webank.eggroll.core.command.CommandClient
 import com.webank.eggroll.core.constant.{CoreConfKeys, ProcessorStatus, SessionConfKeys}
 import com.webank.eggroll.core.containers.JobProcessorTypes
+import com.webank.eggroll.core.deepspeed.job.meta.SubmitJobRequest
 import com.webank.eggroll.core.meta.{ErEndpoint, ErProcessor, ErSessionMeta}
 import com.webank.eggroll.core.session.StaticErConf
 
@@ -26,6 +27,13 @@ object TestAssets {
 //    jobType = JobProcessorTypes.DeepSpeed.toString,
 //    worldSize=2,options = Map("eggroll.container.python.exec"->"python",
 //      "eggroll.container.script.path"->"/data/projects/myeggroll/python/eggroll/mock.py"))
+
+  var submitJobMeta= SubmitJobRequest(sessionId="test_deepspeed"+System.currentTimeMillis(),
+    jobType = JobProcessorTypes.DeepSpeed.toString,
+    worldSize=2,
+    options = Map("eggroll.container.python.exec"->"python",
+      "eggroll.container.script.path"->"/data/projects/myeggroll/python/eggroll/mock.py"))
+
 
   def initConf(): Unit = {
     val confFile = new File("../../conf/eggroll.properties.local")
