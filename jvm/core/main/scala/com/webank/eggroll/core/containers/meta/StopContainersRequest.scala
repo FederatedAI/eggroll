ccf5c10e2840c0348f81519b8e3cb75a23d3a1f4
sagewe <wbwmat@gmail.com>
2023-05-24 12:43:58 +0800

    fix container killing
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- jvm/core/main/scala/com/webank/eggroll/core/containers/meta/StopContainersRequest.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/containers/meta/StopContainersRequest.scala
@@ -1,19 +1,24 @@
 package com.webank.eggroll.core.containers.meta
 
 import com.google.protobuf.ByteString
-import com.webank.eggroll.core.constant.StringConstants
+import com.webank.eggroll.core.meta.Containers
 
+import scala.collection.JavaConverters._
 import scala.language.implicitConversions
 
-case class StopContainersRequest(id: String = StringConstants.EMPTY) {
+case class StopContainersRequest(sessionId: String, containers: Array[Long]) {
 }
 
 object StopContainersRequest {
   implicit def deserialize(byteString: ByteString): StopContainersRequest = {
-    StopContainersRequest()
+    val proto = Containers.StopContainersRequest.parseFrom(byteString)
+    StopContainersRequest(proto.getSessionId, proto.getContainerIdsList.asScala.map(_.toLong)(collection.breakOut))
   }
 
   implicit def serialize(src: StopContainersRequest): Array[Byte] = {
-    Array[Byte]()
+    Containers.StopContainersRequest.newBuilder()
+      .setSessionId(src.sessionId)
+      .addAllContainerIds(src.containers.map(_.asInstanceOf[java.lang.Long]).toSeq.asJava)
+      .build().toByteArray
   }
 }
