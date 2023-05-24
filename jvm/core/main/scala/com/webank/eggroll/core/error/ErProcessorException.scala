12926e6e28b37dc9a4c7c97f38c04a8c49c9ef10
forgive_dengkai <forgive_dengkai@163.com>
2023-05-24 17:40:23 +0800

    resource
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>

---
+++ jvm/core/main/scala/com/webank/eggroll/core/error/ErProcessorException.scala
@@ -0,0 +1,5 @@
+package com.webank.eggroll.core.error
+
+class ErProcessorException (message: String = null,
+                            cause: Throwable = null) extends RuntimeException(message, cause) {
+}
