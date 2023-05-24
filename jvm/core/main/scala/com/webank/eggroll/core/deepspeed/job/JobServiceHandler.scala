366aff161a76f9e94d20d60203fee6a4882f2231
sagewe <wbwmat@gmail.com>
2023-05-24 11:34:55 +0800

    fix device
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- jvm/core/main/scala/com/webank/eggroll/core/deepspeed/job/JobServiceHandler.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/deepspeed/job/JobServiceHandler.scala
@@ -19,55 +19,16 @@ import scala.util.control.Breaks.{break, breakable}
 object JobServiceHandler extends Logging {
   private val smDao = new SessionMetaDao
 
-  ClusterManagerService.registerProcessorCallback(JobProcessorTypes.DeepSpeed.toString, (event: ProcessorEvent) => {
-    new Thread(() => {
-      event match {
-        case ProcessorEvent(eventType, processor) if eventType == ProcessorEventType.PROCESSOR_LOSS =>
-          logDebug(s"processor ${processor.id} lost, kill job ${processor.sessionId}")
-          killJob(processor.sessionId, isTimeout = false)
-      }
-    }).start()
-  })
+  //  ClusterManagerService.registerProcessorCallback(JobProcessorTypes.DeepSpeed.toString, (event: ProcessorEvent) => {
+  //    new Thread(() => {
+  //      event match {
+  //        case ProcessorEvent(eventType, processor) if eventType == ProcessorEventType.PROCESSOR_LOSS =>
+  //          logDebug(s"processor ${processor.id} lost, kill job ${processor.sessionId}")
+  //          killJob(processor.sessionId, isTimeout = false)
+  //      }
+  //    }).start()
+  //  })
 
-  def startSessionWatcher(): Unit = {
-    /*
-    update session status according to processor status
-     */
-    new Thread(
-      () => {
-        while (true) {
-          val sessions = smDao.getSessionMains(ErSessionMeta(status = SessionStatus.ACTIVE))
-          sessions.foreach { session =>
-            // logDebug(s"watch active session: ${session.id}")
-            val sessionProcessors = smDao.getSession(session.id).processors
-            if (sessionProcessors.forall(_.processorType == JobProcessorTypes.DeepSpeed.toString)) {
-              if (sessionProcessors.exists(_.status == ProcessorStatus.ERROR)) {
-                if (sessionProcessors.exists(p => p.status == ProcessorStatus.RUNNING || p.status == ProcessorStatus.NEW)) {
-                  try {
-                    killJob(session.id, isTimeout = false)
-                  } catch {
-                    case e: ErSessionException =>
-                      logError(s"failed to kill session ${session.id}", e)
-                  }
-                }
-
-                smDao.updateSessionMain(session.copy(status = SessionStatus.ERROR), afterCall = defaultSessionCallback)
-
-                logDebug(s"found error processor belongs to session ${session.id}: " +
-                  s"${sessionProcessors.filter(_.status == ProcessorStatus.ERROR).mkString("Array(", ", ", ")")}, " +
-                  s"update session status to `Error`")
-              } else if (sessionProcessors.forall(_.status == ProcessorStatus.FINISHED)) {
-                smDao.updateSessionMain(session.copy(status = SessionStatus.FINISHED), afterCall = defaultSessionCallback)
-                logDebug(s"found all processor belongs to session ${session.id} finished, " +
-                  s"update session status to `Finished`")
-              }
-            }
-          }
-          Thread.sleep(1000)
-        }
-      }
-    ).start()
-  }
 
   def handleJobKill(killJobRequest: KillJobRequest): KillJobResponse = {
     val sessionId = killJobRequest.sessionId
@@ -193,12 +154,14 @@ object JobServiceHandler extends Logging {
         status = ResourceStatus.PRE_ALLOCATED))
     ))
     val resourceApplication = ResourceApplication(
+
       processors = prepareProcessors,
       needDispatch = true,
       resourceExhaustedStrategy = ResourceExhaustedStrategy.WAITING,
-      timeout = submitJobRequest.resourceOptions.timeoutSeconds * 1000,
-      resourceLatch = new CountDownLatch(1),
-      sessionId = sessionId)
+      timeout = submitJobRequest.resourceOptions.timeoutSeconds,
+      sessionId = sessionId,
+      sessionName = JobProcessorTypes.DeepSpeed.toString
+    )
     ClusterResourceManager.submitResourceRequest(resourceApplication)
     var dispatchedProcessors = resourceApplication.getResult()
     logInfo(s"dispatchedProcessor: ${dispatchedProcessors.mkString("Array(", ", ", ")")}")
@@ -240,14 +203,26 @@ object JobServiceHandler extends Logging {
             files = submitJobRequest.files,
             zippedFiles = submitJobRequest.zippedFiles,
             deepspeedConfigs = processors.map { p =>
+              val cudaVisibleDevices = p.options.getOrDefault("cudaVisibleDevices", "-1").split(",").map(_.toInt)
+              if (cudaVisibleDevices.length < 1 || cudaVisibleDevices.exists(_ < 0)) {
+                throw new IllegalArgumentException(s"cudaVisibleDevices is not set or invalid: ${p.options.get("cudaVisibleDevices")}")
+              }
+              val localRank = p.options.getOrDefault("localRank", "-1").toInt
+              if (localRank < 0) {
+                throw new IllegalArgumentException(s"localRank is not set or invalid: ${p.options.get("localRank")}")
+              }
+              val rank = p.options.getOrDefault("globalRank", "-1").toInt
+              if (rank < 0) {
+                throw new IllegalArgumentException(s"globalRank is not set or invalid: ${p.options.get("globalRank")}")
+              }
               p.id -> DeepspeedContainerConfig(
-                cudaVisibleDevices = p.options.getOrDefault("cudaVisibleDevices", "0,1").split(",").map(_.toInt),
+                cudaVisibleDevices = cudaVisibleDevices,
                 worldSize = worldSize,
                 crossRank = crossRank,
                 crossSize = crossSize,
                 localSize = localSize,
-                localRank = p.options.get("localRank").toInt,
-                rank = p.options.get("globalRank").toInt,
+                localRank = localRank,
+                rank = rank,
                 storePrefix = sessionId
               )
             }(collection.breakOut),
@@ -306,12 +281,12 @@ object JobServiceHandler extends Logging {
           e.printStackTrace()
       }
     }
-//    if (isTimeout) {
-//      smDao.updateSessionStatus(sessionId = sessionId, status = SessionStatus.NEW_TIMEOUT)
-//    } else {
-//      smDao.updateSessionStatus(sessionId = sessionId, status = SessionStatus.KILLED)
-//    }
+    //    if (isTimeout) {
+    //      smDao.updateSessionStatus(sessionId = sessionId, status = SessionStatus.NEW_TIMEOUT)
+    //    } else {
+    //      smDao.updateSessionStatus(sessionId = sessionId, status = SessionStatus.KILLED)
+    //    }
 
-    smDao.updateSessionMain(sessionMeta.copy(status = SessionStatus.ERROR) ,afterCall=defaultSessionCallback)
+    smDao.updateSessionMain(sessionMeta.copy(status = SessionStatus.ERROR), afterCall = defaultSessionCallback)
   }
 }
