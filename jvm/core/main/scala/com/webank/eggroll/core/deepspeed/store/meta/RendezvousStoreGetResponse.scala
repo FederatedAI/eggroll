799ec1e49fbdde7453aa818a5c7bbcc6392f1eb5
sagewe <wbwmat@gmail.com>
2023-05-26 11:48:53 +0800

    fix store timeout
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- jvm/core/main/scala/com/webank/eggroll/core/deepspeed/store/meta/RendezvousStoreGetResponse.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/deepspeed/store/meta/RendezvousStoreGetResponse.scala
@@ -5,19 +5,21 @@ import com.webank.eggroll.core.meta.Deepspeed
 
 import scala.language.implicitConversions
 
-case class RendezvousStoreGetResponse(value: V)
+case class RendezvousStoreGetResponse(value: V, isTimeout: Boolean)
 
 object RendezvousStoreGetResponse {
   implicit def serialize(src: RendezvousStoreGetResponse): Array[Byte] = {
     val builder = Deepspeed.StoreGetResponse.newBuilder()
       .setValue(ByteString.copyFrom(src.value.toArray))
+      .setIsTimeout(src.isTimeout)
     builder.build().toByteArray
   }
 
   implicit def deserialize(byteString: ByteString): RendezvousStoreGetResponse = {
     val src = Deepspeed.StoreGetResponse.parseFrom(byteString)
     RendezvousStoreGetResponse(
-      value = src.getValue.toByteArray.toVector
+      value = src.getValue.toByteArray.toVector,
+      isTimeout = src.getIsTimeout
     )
   }
 }
