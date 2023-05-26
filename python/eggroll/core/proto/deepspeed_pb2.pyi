679557f1b3e2575b2a6977ae6f3ee07ae49cf2f8
sagewe <wbwmat@gmail.com>
2023-05-26 11:48:53 +0800

    fix store timeout
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- python/eggroll/core/proto/deepspeed_pb2.pyi
+++ python/eggroll/core/proto/deepspeed_pb2.pyi
@@ -78,13 +78,16 @@ class StoreGetResponse(google.protobuf.message.Message):
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
 
     VALUE_FIELD_NUMBER: builtins.int
+    IS_TIMEOUT_FIELD_NUMBER: builtins.int
     value: builtins.bytes
+    is_timeout: builtins.bool
     def __init__(
         self,
         *,
         value: builtins.bytes = ...,
+        is_timeout: builtins.bool = ...,
     ) -> None: ...
-    def ClearField(self, field_name: typing_extensions.Literal["value", b"value"]) -> None: ...
+    def ClearField(self, field_name: typing_extensions.Literal["is_timeout", b"is_timeout", "value", b"value"]) -> None: ...
 
 global___StoreGetResponse = StoreGetResponse
 
