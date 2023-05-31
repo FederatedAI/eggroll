a9dc8f29e09b9a60800e0eb5a25c20248ec9971c
sagewe <wbwmat@gmail.com>
2023-05-31 15:37:29 +0800

    add poison status
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- jvm/core/main/scala/com/webank/eggroll/core/containers/container/ContainerStatus.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/containers/container/ContainerStatus.scala
@@ -1,5 +1,5 @@
 package com.webank.eggroll.core.containers.container;
 
 object ContainerStatus extends Enumeration {
-  val Pending, Started, Failed, Success, Exception = Value
+  val Pending, Started, Failed, Poison, Success, Exception = Value
 }
