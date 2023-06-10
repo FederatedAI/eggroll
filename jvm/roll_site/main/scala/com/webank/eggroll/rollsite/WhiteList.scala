7fb8e2b13b35dba39027a567066f033ec9b875cf
elvis-xiao <xqxiao023@163.com>
2023-06-10 23:08:39 +0800

    support network segments for whitelist configuration
    Signed-off-by: elvis-xiao <xqxiao023@163.com>

--- jvm/roll_site/main/scala/com/webank/eggroll/rollsite/WhiteList.scala
+++ jvm/roll_site/main/scala/com/webank/eggroll/rollsite/WhiteList.scala
@@ -3,6 +3,7 @@ package com.webank.eggroll.rollsite
 import com.webank.eggroll.core.constant.RollSiteConfKeys
 import io.grpc._
 import org.apache.commons.lang3.StringUtils
+import scala.collection.mutable.ArrayBuffer
 
 object WhiteList {
   private var whiteList: Array[String] = Array()
@@ -15,7 +16,24 @@ object WhiteList {
   def init(str: String): Unit = {
     if (!StringUtils.isBlank(str)) {
       val whiteListArray = str.split("\\,")
-      this.whiteList = whiteListArray
+      val arrBuf = ArrayBuffer[String]()
+      whiteListArray.foreach((elem: String) => {
+        // e.g: 127.0.0.1/5
+        if (elem.contains("/")){
+          val ipSeg = elem.split("/")
+          if (ipSeg.length != 2) {
+            throw new IllegalArgumentException("whitelist config error: %s".format(elem))
+          }
+          val startSeg = ipSeg(0)
+          val lastDotIndex = startSeg.lastIndexOf(".")
+          for (i <- startSeg.substring(lastDotIndex +1).toInt to ipSeg(1).toInt) {
+            arrBuf.append(startSeg.substring(0, lastDotIndex+1) + i)
+          }
+        }else{
+          arrBuf.append(elem)
+        }
+      })
+      this.whiteList = arrBuf.toArray
     }
   }
 
