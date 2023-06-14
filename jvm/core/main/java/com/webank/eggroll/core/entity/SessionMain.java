903ce65c3ace1c3921b32fadf9106daca424e70b
v_wbxiongli <740332065@qq.com>
2023-06-14 21:29:47 +0800

    add entity

---
+++ jvm/core/main/java/com/webank/eggroll/core/entity/SessionMain.java
@@ -0,0 +1,100 @@
+package com.webank.eggroll.core.entity;
+
+import java.util.Date;
+
+public class SessionMain {
+    private String sessionId;
+
+    private String name;
+
+    private String status;
+
+    private String tag;
+
+    private Integer totalProcCount;
+
+    private Integer activeProcCount;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public SessionMain(String sessionId, String name, String status, String tag, Integer totalProcCount, Integer activeProcCount, Date createdAt, Date updatedAt) {
+        this.sessionId = sessionId;
+        this.name = name;
+        this.status = status;
+        this.tag = tag;
+        this.totalProcCount = totalProcCount;
+        this.activeProcCount = activeProcCount;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public SessionMain() {
+        super();
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId == null ? null : sessionId.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag == null ? null : tag.trim();
+    }
+
+    public Integer getTotalProcCount() {
+        return totalProcCount;
+    }
+
+    public void setTotalProcCount(Integer totalProcCount) {
+        this.totalProcCount = totalProcCount;
+    }
+
+    public Integer getActiveProcCount() {
+        return activeProcCount;
+    }
+
+    public void setActiveProcCount(Integer activeProcCount) {
+        this.activeProcCount = activeProcCount;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
