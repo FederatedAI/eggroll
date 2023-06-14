b694a5fda96a69b96e549801937aecb44c0e072b
v_wbxiongli <740332065@qq.com>
2023-06-14 21:29:47 +0800

    add entity

---
+++ jvm/core/main/java/com/webank/eggroll/core/entity/SessionProcessor.java
@@ -0,0 +1,144 @@
+package com.webank.eggroll.core.entity;
+
+import java.util.Date;
+
+public class SessionProcessor {
+    private Long processorId;
+
+    private String sessionId;
+
+    private Integer serverNodeId;
+
+    private String processorType;
+
+    private String status;
+
+    private String tag;
+
+    private String commandEndpoint;
+
+    private String transferEndpoint;
+
+    private String processorOption;
+
+    private Integer pid;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public SessionProcessor(Long processorId, String sessionId, Integer serverNodeId, String processorType, String status, String tag, String commandEndpoint, String transferEndpoint, String processorOption, Integer pid, Date createdAt, Date updatedAt) {
+        this.processorId = processorId;
+        this.sessionId = sessionId;
+        this.serverNodeId = serverNodeId;
+        this.processorType = processorType;
+        this.status = status;
+        this.tag = tag;
+        this.commandEndpoint = commandEndpoint;
+        this.transferEndpoint = transferEndpoint;
+        this.processorOption = processorOption;
+        this.pid = pid;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public SessionProcessor() {
+        super();
+    }
+
+    public Long getProcessorId() {
+        return processorId;
+    }
+
+    public void setProcessorId(Long processorId) {
+        this.processorId = processorId;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId == null ? null : sessionId.trim();
+    }
+
+    public Integer getServerNodeId() {
+        return serverNodeId;
+    }
+
+    public void setServerNodeId(Integer serverNodeId) {
+        this.serverNodeId = serverNodeId;
+    }
+
+    public String getProcessorType() {
+        return processorType;
+    }
+
+    public void setProcessorType(String processorType) {
+        this.processorType = processorType == null ? null : processorType.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag == null ? null : tag.trim();
+    }
+
+    public String getCommandEndpoint() {
+        return commandEndpoint;
+    }
+
+    public void setCommandEndpoint(String commandEndpoint) {
+        this.commandEndpoint = commandEndpoint == null ? null : commandEndpoint.trim();
+    }
+
+    public String getTransferEndpoint() {
+        return transferEndpoint;
+    }
+
+    public void setTransferEndpoint(String transferEndpoint) {
+        this.transferEndpoint = transferEndpoint == null ? null : transferEndpoint.trim();
+    }
+
+    public String getProcessorOption() {
+        return processorOption;
+    }
+
+    public void setProcessorOption(String processorOption) {
+        this.processorOption = processorOption == null ? null : processorOption.trim();
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ jvm/core/main/java/com/webank/eggroll/core/entity/StoreLocator.java
@@ -0,0 +1,144 @@
+package com.webank.eggroll.core.entity;
+
+import java.util.Date;
+
+public class StoreLocator {
+    private Long storeLocatorId;
+
+    private String storeType;
+
+    private String namespace;
+
+    private String name;
+
+    private String path;
+
+    private Integer totalPartitions;
+
+    private String partitioner;
+
+    private String serdes;
+
+    private Integer version;
+
+    private String status;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public StoreLocator(Long storeLocatorId, String storeType, String namespace, String name, String path, Integer totalPartitions, String partitioner, String serdes, Integer version, String status, Date createdAt, Date updatedAt) {
+        this.storeLocatorId = storeLocatorId;
+        this.storeType = storeType;
+        this.namespace = namespace;
+        this.name = name;
+        this.path = path;
+        this.totalPartitions = totalPartitions;
+        this.partitioner = partitioner;
+        this.serdes = serdes;
+        this.version = version;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public StoreLocator() {
+        super();
+    }
+
+    public Long getStoreLocatorId() {
+        return storeLocatorId;
+    }
+
+    public void setStoreLocatorId(Long storeLocatorId) {
+        this.storeLocatorId = storeLocatorId;
+    }
+
+    public String getStoreType() {
+        return storeType;
+    }
+
+    public void setStoreType(String storeType) {
+        this.storeType = storeType == null ? null : storeType.trim();
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace == null ? null : namespace.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public Integer getTotalPartitions() {
+        return totalPartitions;
+    }
+
+    public void setTotalPartitions(Integer totalPartitions) {
+        this.totalPartitions = totalPartitions;
+    }
+
+    public String getPartitioner() {
+        return partitioner;
+    }
+
+    public void setPartitioner(String partitioner) {
+        this.partitioner = partitioner == null ? null : partitioner.trim();
+    }
+
+    public String getSerdes() {
+        return serdes;
+    }
+
+    public void setSerdes(String serdes) {
+        this.serdes = serdes == null ? null : serdes.trim();
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
