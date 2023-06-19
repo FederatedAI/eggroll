e367ef707cd9e1d409b959ecbd224f553d457192
forgive_dengkai <forgive_dengkai@163.com>
2023-06-19 14:15:38 +0800

    new function
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>

--- jvm/core/main/scala/com/webank/eggroll/core/ApplicationContext.scala
+++ jvm/core/main/scala/com/webank/eggroll/core/ApplicationContext.scala
@@ -1,7 +1,10 @@
 package com.webank.eggroll.core
 
+import com.webank.eggroll.clustermanager.statemechine.ProcessorStateMechine
 import org.springframework.context.ApplicationContext
 
 object ContextHolder {
-   var  context: ApplicationContext =null
+   var  context: ApplicationContext = null
+ // var mechine= context.getBean("",ProcessorStateMechine)
+
 }
