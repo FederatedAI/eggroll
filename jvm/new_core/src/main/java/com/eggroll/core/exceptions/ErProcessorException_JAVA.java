cec659c0817b7686b25dd9f89d2ec136801e8bd6
v_wbxiongli <740332065@qq.com>
2023-08-01 15:44:54 +0800

    add
    Signed-off-by: v_wbxiongli <740332065@qq.com>

---
+++ jvm/new_core/src/main/java/com/eggroll/core/exceptions/ErProcessorException_JAVA.java
@@ -0,0 +1,12 @@
+package com.eggroll.core.exceptions;
+
+public class ErProcessorException_JAVA extends EggRollBaseException{
+
+    public ErProcessorException_JAVA(String message) {
+        super(message);
+    }
+
+    public ErProcessorException_JAVA(int exCode, String message) {
+        super(exCode, message);
+    }
+}
