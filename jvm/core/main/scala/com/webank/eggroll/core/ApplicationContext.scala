6c3758d3b67f15774856ecdac0a7862badc777a7
forgive_dengkai <forgive_dengkai@163.com>
2023-08-04 09:37:55 +0800

    new  eggroll
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>

--- jvm/core/main/scala/com/webank/eggroll/core/ApplicationContext.scala
+++
@@ -1,9 +0,0 @@
-package com.webank.eggroll.core
-
-import org.springframework.context.ApplicationContext
-
-object ContextHolder {
-   var  context: ApplicationContext = null
- // var mechine= context.getBean("",ProcessorStateMechine)
-
-}
--- jvm/core/main/scala/com/webank/eggroll/core/error/ErProcessorException.scala
+++
@@ -1,5 +0,0 @@
-package com.webank.eggroll.core.error
-
-class ErProcessorException (message: String = null,
-                            cause: Throwable = null) extends RuntimeException(message, cause) {
-}
