82dd4cd6641213e3017c57e1e7988d556f40456d
v_wbxiongli <740332065@qq.com>
2023-08-04 11:18:42 +0800

    update
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/dao/mapper/SessionMainMapper.scala
+++
@@ -1,6 +0,0 @@
-//package com.webank.eggroll.clustermanager.dao.mapper
-//
-//import com.baomidou.mybatisplus.core.mapper.BaseMapper
-//import com.webank.eggroll.clustermanager.entity.SessionMain
-//
-//trait SessionMainMapper extends BaseMapper[SessionMain] {}
