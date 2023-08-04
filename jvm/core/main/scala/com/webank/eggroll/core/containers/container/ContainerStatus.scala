44d706404891fc5e8548cc0f101e1dcb28b0d95a
forgive_dengkai <forgive_dengkai@163.com>
2023-08-04 09:37:55 +0800

    node_manager cmd-start handle

--- jvm/core/main/scala/com/webank/eggroll/core/containers/container/ContainerStatus.scala
+++
@@ -1,5 +0,0 @@
-package com.webank.eggroll.core.containers.container;
-
-object ContainerStatus extends Enumeration {
-  val Pending, Started, Failed, Poison, Success, Exception = Value
-}
--- jvm/node_manager/src/main/java/com/webank/eggroll/nodemanager/processor/DefaultProcessorManager.java
+++ jvm/node_manager/src/main/java/com/webank/eggroll/nodemanager/processor/DefaultProcessorManager.java
@@ -1,12 +1,20 @@
 package com.webank.eggroll.nodemanager.processor;
 
+import com.eggroll.core.config.Dict;
 import com.eggroll.core.context.Context;
 import com.eggroll.core.pojo.ErSessionMeta;
+import com.webank.eggroll.nodemanager.service.ContainerService;
+
+import javax.annotation.Resource;
 
 public class DefaultProcessorManager implements ProcessorManager{
+
+    @Resource
+    private ContainerService containerService;
+
     @Override
     public ErSessionMeta startContainers(Context context, ErSessionMeta sessionMeta) {
-        return null;
+        return containerService.operateContainers(sessionMeta, Dict.NODE_CMD_START);
     }
 
     @Override
@@ -23,4 +31,5 @@ public class DefaultProcessorManager implements ProcessorManager{
     public ErSessionMeta heartbeat(Context context, ErSessionMeta sessionMeta) {
         return null;
     }
+
 }
