f04ca181f65781fd55bf967f2f2a269403e388b9
v_wbxiongli <740332065@qq.com>
2023-08-10 17:47:52 +0800

    add
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/grpc/CommandServiceProvider.java
+++ jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/grpc/CommandServiceProvider.java
@@ -3,42 +3,44 @@ package com.webank.eggroll.clustermanager.grpc;
 import com.eggroll.core.grpc.URI;
 import com.eggroll.core.invoke.InvokeInfo;
 import com.eggroll.core.pojo.*;
-import com.google.common.collect.Lists;
 import com.google.protobuf.ByteString;
+import com.webank.eggroll.clustermanager.dao.impl.ServerNodeService;
+import com.webank.eggroll.clustermanager.dao.impl.StoreCrudOperator;
 import com.webank.eggroll.clustermanager.session.DefaultSessionManager;
 import com.webank.eggroll.core.command.Command;
 import com.webank.eggroll.core.command.CommandServiceGrpc;
 import io.grpc.stub.StreamObserver;
-import org.apache.ibatis.annotations.Param;
-import org.checkerframework.checker.units.qual.A;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static com.eggroll.core.grpc.CommandUri.*;
 
 @Service
-public class CommandServiceProvider  extends CommandServiceGrpc.CommandServiceImplBase implements InitializingBean {
+public class CommandServiceProvider extends CommandServiceGrpc.CommandServiceImplBase implements InitializingBean {
 
     Logger logger = LoggerFactory.getLogger(CommandServiceProvider.class);
 
     @Autowired
-    DefaultSessionManager  defaultSessionManager;
+    DefaultSessionManager defaultSessionManager;
+    @Autowired
+    ServerNodeService serverNodeService;
+    @Autowired
+    StoreCrudOperator storeCrudOperator;
 
 
-
-     public void call(Command.CommandRequest request,
-                      StreamObserver<Command.CommandResponse> responseObserver){
-        String  uri = request.getUri();
-        byte[]  resultBytes = dispatch(uri,request.getArgsList().get(0).toByteArray());
-        Command.CommandResponse.Builder  responseBuilder = Command.CommandResponse.newBuilder();
+    public void call(Command.CommandRequest request,
+                     StreamObserver<Command.CommandResponse> responseObserver) {
+        String uri = request.getUri();
+        byte[] resultBytes = dispatch(uri, request.getArgsList().get(0).toByteArray());
+        Command.CommandResponse.Builder responseBuilder = Command.CommandResponse.newBuilder();
         responseBuilder.setId(request.getId());
         responseBuilder.addResults(ByteString.copyFrom(resultBytes));
         responseObserver.onNext(responseBuilder.build());
@@ -46,18 +48,18 @@ public class CommandServiceProvider  extends CommandServiceGrpc.CommandServiceIm
     }
 
 
-    private ConcurrentHashMap<String , InvokeInfo>   uriMap = new ConcurrentHashMap();
+    private ConcurrentHashMap<String, InvokeInfo> uriMap = new ConcurrentHashMap();
 
-    public  byte[]  dispatch(String uri ,byte[] data){
-        InvokeInfo invokeInfo =  uriMap.get(uri);
-        logger.info("request {} invoke {}",uri,invokeInfo);
-        if(invokeInfo ==null){
-            throw new  RuntimeException("invalid request : "+uri);
+    public byte[] dispatch(String uri, byte[] data) {
+        InvokeInfo invokeInfo = uriMap.get(uri);
+        logger.info("request {} invoke {}", uri, invokeInfo);
+        if (invokeInfo == null) {
+            throw new RuntimeException("invalid request : " + uri);
         }
         try {
-            RpcMessage  rpcMessage = (RpcMessage)invokeInfo.getParamClass().newInstance();
+            RpcMessage rpcMessage = (RpcMessage) invokeInfo.getParamClass().newInstance();
             rpcMessage.deserialize(data);
-            RpcMessage response =  (RpcMessage)invokeInfo.getMethod().invoke(invokeInfo.getObject(),rpcMessage);
+            RpcMessage response = (RpcMessage) invokeInfo.getMethod().invoke(invokeInfo.getObject(), rpcMessage);
             return response.serialize();
 
         } catch (InstantiationException e) {
@@ -71,63 +73,76 @@ public class CommandServiceProvider  extends CommandServiceGrpc.CommandServiceIm
 
     }
 
-    @URI(value=getServerNode)
-    public ErServerNode getServerNodeServiceName(ErServerNode  erServerNode){
-        return  null;
+    @URI(value = getServerNode)
+    public ErServerNode getServerNodeServiceName(ErServerNode erServerNode) {
+        List<ErServerNode> nodeList = serverNodeService.getListByErServerNode(erServerNode);
+        return nodeList.size() > 0 ? nodeList.get(0) : null;
     }
-    @URI(value=getServerNodes)
-    public ErServerCluster getServerNodesServiceName(ErServerNode  erServerNode){
-        return  null;
-    }
-    @URI(value=getOrCreateServerNode)
-    public ErServerNode  getOrCreateServerNode(ErServerNode  erServerNode){
-        return null;
-    }
-    @URI(value=createOrUpdateServerNode)
-    public  ErServerNode createOrUpdateServerNode (ErServerNode  erServerNode){
+
+    @URI(value = getServerNodes)
+    public ErServerCluster getServerNodesServiceName(ErServerNode erServerNode) {
         return null;
     }
 
-    @URI(value=getStore)
-    public  ErStore  getStore(ErStore erStore){
+    @URI(value = getOrCreateServerNode)
+    public ErServerNode getOrCreateServerNode(ErServerNode erServerNode) {
         return null;
     }
-    @URI(value=getOrCreateStore)
-    public  ErStore  getOrCreateStore(ErStore  erStore){
-        return null;
-    }
-    @URI(value=deleteStore)
-    public ErStore deleteStore(ErStore erStore){
-        return  null;
-    }
-    @URI(value=getStoreFromNamespace)
-    public ErStore getStoreFromNamespace(ErStore  erStore){
-        return null;
-    }
-    @URI(value=getOrCreateSession)
-    public ErSessionMeta  getOrCreateSession(ErSessionMeta sessionMeta){
-        return defaultSessionManager.getOrCreateSession(null,sessionMeta);
-    }
-    @URI(value=getSession)
-    public ErSessionMeta  getSession(ErSessionMeta sessionMeta){
-        return defaultSessionManager.getSession(null,sessionMeta);
-    }
-    @URI(value=heartbeat)
-    public ErProcessor heartbeat(ErProcessor erProcessor){
 
-        return defaultSessionManager.heartbeat(null,erProcessor);
+    @URI(value = createOrUpdateServerNode)
+    public ErServerNode createOrUpdateServerNode(ErServerNode erServerNode) {
+        return null;
     }
-    @URI(value=stopSession)
-    public ErSessionMeta stopSession(ErSessionMeta erSessionMeta){
-        return defaultSessionManager.stopSession(null,erSessionMeta);
+
+    @URI(value = getStore)
+    public ErStore getStore(ErStore erStore) {
+        return storeCrudOperator.doGetStore(erStore);
     }
-    @URI(value=killSession)
-    public ErSessionMeta killSession(ErSessionMeta erSessionMeta){
-        return defaultSessionManager.killSession(null,erSessionMeta);
+
+    @URI(value = getOrCreateStore)
+    public ErStore getOrCreateStore(ErStore erStore) {
+        return storeCrudOperator.doGetOrCreateStore(erStore);
     }
-    @URI(value=killAllSessions)
-    public ErSessionMeta killAllSession(ErSessionMeta erSessionMeta){
-        return defaultSessionManager.killAllSessions(null,erSessionMeta);
+
+    @URI(value = deleteStore)
+    public ErStore deleteStore(ErStore erStore) {
+        return storeCrudOperator.doDeleteStore(erStore);
+    }
+
+    @URI(value = getStoreFromNamespace)
+    public ErStoreList getStoreFromNamespace(ErStore erStore) {
+        return storeCrudOperator.getStoreFromNamespace(erStore);
+    }
+
+    @URI(value = getOrCreateSession)
+    public ErSessionMeta getOrCreateSession(ErSessionMeta sessionMeta) {
+        return defaultSessionManager.getOrCreateSession(null, sessionMeta);
+    }
+
+    @URI(value = getSession)
+    public ErSessionMeta getSession(ErSessionMeta sessionMeta) {
+        return defaultSessionManager.getSession(null, sessionMeta);
+    }
+
+    @URI(value = heartbeat)
+    public ErProcessor heartbeat(ErProcessor erProcessor) {
+
+        return defaultSessionManager.heartbeat(null, erProcessor);
+    }
+
+    @URI(value = stopSession)
+    public ErSessionMeta stopSession(ErSessionMeta erSessionMeta) {
+        return defaultSessionManager.stopSession(null, erSessionMeta);
+    }
+
+    @URI(value = killSession)
+    public ErSessionMeta killSession(ErSessionMeta erSessionMeta) {
+        return defaultSessionManager.killSession(null, erSessionMeta);
+    }
+
+    @URI(value = killAllSessions)
+    public ErSessionMeta killAllSession(ErSessionMeta erSessionMeta) {
+        return defaultSessionManager.killAllSessions(null, erSessionMeta);
     }
 
 
@@ -138,10 +153,10 @@ public class CommandServiceProvider  extends CommandServiceGrpc.CommandServiceIm
 
     }
 
-    private void doRegister(String uri,Object  service,Method  method,Class  paramClass){
-        InvokeInfo  invokeInfo = new InvokeInfo(uri,service,method,paramClass);
-        logger.info("register uri {}",invokeInfo);
-        this.uriMap.put(uri,invokeInfo);
+    private void doRegister(String uri, Object service, Method method, Class paramClass) {
+        InvokeInfo invokeInfo = new InvokeInfo(uri, service, method, paramClass);
+        logger.info("register uri {}", invokeInfo);
+        this.uriMap.put(uri, invokeInfo);
     }
 
     private void register(Object service) {
@@ -155,16 +170,16 @@ public class CommandServiceProvider  extends CommandServiceGrpc.CommandServiceIm
 
             URI uri = method.getDeclaredAnnotation(URI.class);
 
-            if(uri!=null){
+            if (uri != null) {
                 Class[] types = method.getParameterTypes();
-                if(types.length>0){
-                   Class  paramClass = types[0];
-                    System.err.println("paramClass "+paramClass);
-                   if(RpcMessage.class.isAssignableFrom(paramClass)){
-                       doRegister(uri.value(),service,method,paramClass);
-                   }else{
+                if (types.length > 0) {
+                    Class paramClass = types[0];
+                    System.err.println("paramClass " + paramClass);
+                    if (RpcMessage.class.isAssignableFrom(paramClass)) {
+                        doRegister(uri.value(), service, method, paramClass);
+                    } else {
 //                       System.err.println("false "+paramClass);
-                   }
+                    }
                 }
 
             }
@@ -172,7 +187,6 @@ public class CommandServiceProvider  extends CommandServiceGrpc.CommandServiceIm
         }
 
     }
-
 
 
 }
