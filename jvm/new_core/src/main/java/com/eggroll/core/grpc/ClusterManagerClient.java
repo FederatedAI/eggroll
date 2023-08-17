360433172f178121b34ccf2960fdf056c55d7551
v_wbxiongli <740332065@qq.com>
2023-08-17 17:10:06 +0800

    update
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/new_core/src/main/java/com/eggroll/core/grpc/ClusterManagerClient.java
+++ jvm/new_core/src/main/java/com/eggroll/core/grpc/ClusterManagerClient.java
@@ -10,38 +10,58 @@ import static com.eggroll.core.grpc.CommandUri.*;
 
 public class ClusterManagerClient {
 
-    CommandClient  cc;
+    CommandClient cc;
     ErEndpoint endpoint;
-    public  ClusterManagerClient(ErEndpoint endpoint){
-        if (endpoint == null )
+
+    public ClusterManagerClient(ErEndpoint endpoint) {
+        if (endpoint == null)
             throw new IllegalArgumentException("failed to create NodeManagerClient for endpoint: " + endpoint);
         this.endpoint = endpoint;
         cc = new CommandClient();
     }
 
 
-
     public ErProcessor hearbeat(ErProcessor processor) {
-        byte[] responseData = cc.call(endpoint, heartbeat,processor.serialize());
+        byte[] responseData = cc.call(endpoint, heartbeat, processor.serialize());
         ErProcessor response = new ErProcessor();
         response.deserialize(responseData);
         return response;
     }
 
-    public   ErSessionMeta   getOrCreateSession(ErSessionMeta  erSessionMeta){
-        byte[] responseData = cc.call(endpoint, getOrCreateSession,erSessionMeta.serialize());
+    public ErSessionMeta getOrCreateSession(ErSessionMeta erSessionMeta) {
+        byte[] responseData = cc.call(endpoint, getOrCreateSession, erSessionMeta.serialize());
         ErSessionMeta response = new ErSessionMeta();
         response.deserialize(responseData);
-        return  response;
+        return response;
     }
 
+    public ErSessionMeta getSession(ErSessionMeta erSessionMeta) {
+        byte[] responseData = cc.call(endpoint, getSession, erSessionMeta.serialize());
+        ErSessionMeta response = new ErSessionMeta();
+        response.deserialize(responseData);
+        return response;
+    }
+
+    public ErSessionMeta killSession(ErSessionMeta erSessionMeta) {
+        byte[] responseData = cc.call(endpoint, killSession, erSessionMeta.serialize());
+        ErSessionMeta response = new ErSessionMeta();
+        response.deserialize(responseData);
+        return response;
+    }
+
+    public ErSessionMeta killAllSession() {
+        byte[] responseData = cc.call(endpoint, killAllSessions, null);
+        ErSessionMeta response = new ErSessionMeta();
+        response.deserialize(responseData);
+        return response;
+    }
 
 
 //    def nodeHeartbeat(node: ErNodeHeartbeat): ErNodeHeartbeat =
 //    cc.call[ErNodeHeartbeat](ManagerCommands.nodeHeartbeat, node)
 
-    public ErNodeHeartbeat nodeHeartbeat(ErNodeHeartbeat node){
-        byte[] responseData = cc.call(endpoint, nodeHeartbeat,node.serialize());
+    public ErNodeHeartbeat nodeHeartbeat(ErNodeHeartbeat node) {
+        byte[] responseData = cc.call(endpoint, nodeHeartbeat, node.serialize());
         ErNodeHeartbeat response = new ErNodeHeartbeat();
         response.deserialize(responseData);
 
