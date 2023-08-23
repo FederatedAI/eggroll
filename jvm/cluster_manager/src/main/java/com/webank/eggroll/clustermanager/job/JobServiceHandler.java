eb5657052a9d1821a34a19c0ea81e58a894a18e8
v_wbxiongli <740332065@qq.com>
2023-08-23 17:45:25 +0800

    update
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/job/JobServiceHandler.java
+++ jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/job/JobServiceHandler.java
@@ -1,24 +1,26 @@
 package com.webank.eggroll.clustermanager.job;
 
+import com.eggroll.core.config.Dict;
+import com.eggroll.core.constant.ProcessorStatus;
+import com.eggroll.core.constant.ResourceStatus;
 import com.eggroll.core.constant.SessionStatus;
+import com.eggroll.core.exceptions.ErSessionException;
 import com.eggroll.core.grpc.NodeManagerClient;
-import com.eggroll.core.pojo.ErProcessor;
-import com.eggroll.core.pojo.ErServerNode;
-import com.eggroll.core.pojo.ErSessionMeta;
-import com.eggroll.core.pojo.KillContainersRequest;
+import com.eggroll.core.pojo.*;
+import com.eggroll.core.utils.JsonUtil;
 import com.webank.eggroll.clustermanager.cluster.ClusterResourceManager;
 import com.webank.eggroll.clustermanager.dao.impl.ServerNodeService;
 import com.webank.eggroll.clustermanager.dao.impl.SessionMainService;
+import javafx.util.Pair;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Triple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 
@@ -68,8 +70,7 @@ public class JobServiceHandler {
     }
 
 //    public SubmitJobResponse handleSubmit(SubmitJobRequest submitJobMeta) {
-//        JobProcessorTypes jobType = JobProcessorTypes.fromString(submitJobMeta.getJobType());
-//        if (jobType == JobProcessorTypes.DeepSpeed) {
+//        if (JobProcessorTypes.DeepSpeed.name().equals(submitJobMeta.getJobType())) {
 //            return handleDeepspeedSubmit(submitJobMeta);
 //        } else {
 //            throw new IllegalArgumentException("unsupported job type: " + submitJobMeta.getJobType());
@@ -93,5 +94,152 @@ public class JobServiceHandler {
 //        String sessionId = stopJobRequest.getSessionId();
 //        killJob(sessionId, false);
 //        return new StopJobResponse(sessionId);
+//    }
+//
+//    private SubmitJobResponse handleDeepspeedSubmit(SubmitJobRequest submitJobRequest) throws InterruptedException {
+//        String sessionId = submitJobRequest.getSessionId();
+//        int worldSize = submitJobRequest.getWorldSize();
+//
+//        // prepare processors
+//        List<ErProcessor> prepareProcessors = new ArrayList<>();
+//        for (int i = 0; i < worldSize; i++) {
+//            ErProcessor erProcessor = new ErProcessor();
+//            erProcessor.setProcessorType(JobProcessorTypes.DeepSpeed.name());
+//            erProcessor.setStatus(ProcessorStatus.NEW.name());
+//
+//            ErResource erResource = new ErResource();
+//            erResource.setResourceType(Dict.VGPU_CORE);
+//            erResource.setAllocated(1L);
+//            erResource.setStatus(ResourceStatus.PRE_ALLOCATED.name());
+//            erProcessor.getResources().add(erResource);
+//
+//            prepareProcessors.add(erProcessor);
+//        }
+//
+//        ResourceApplication resourceApplication = new ResourceApplication();
+//        resourceApplication.setSortByResourceType(Dict.VCPU_CORE);
+//        resourceApplication.setProcessors(prepareProcessors);
+//        resourceApplication.setResourceExhaustedStrategy(Dict.WAITING);
+//        resourceApplication.setTimeout(submitJobRequest.getResourceOptions().getTimeoutSeconds() * 1000);
+//        resourceApplication.setSessionId(sessionId);
+//        resourceApplication.setSessionName(JobProcessorTypes.DeepSpeed.toString());
+//        log.info("submitting resource request: " + resourceApplication + ", " + resourceApplication.hashCode());
+//
+//        clusterResourceManager.submitResourceRequest(resourceApplication);
+//        List<Map<ErProcessor, ErServerNode>> dispatchedProcessorList = resourceApplication.getResult();
+//        log.info("submitted resource request: " + resourceApplication + ", " + resourceApplication.hashCode());
+//        log.info("dispatchedProcessor: " + JsonUtil.object2Json(dispatchedProcessorList));
+//
+//        try {
+//            //锁不能移到分配资源之前，会造成死锁
+//            clusterResourceManager.lockSession(sessionId);
+//            if (!clusterResourceManager.getKillJobMap().containsKey(sessionId)) {
+//                ErSessionMeta registeredSessionMeta = sessionMainService.getSession(submitJobRequest.getSessionId());
+//                List<Pair<Map<ErProcessor, ErServerNode>, ErProcessor>> pariList = new ArrayList<>();
+//                for (int i = 0; i < dispatchedProcessorList.size(); i++) {
+//                    Map<ErProcessor, ErServerNode> erProcessorErServerNodeMap = dispatchedProcessorList.get(i);
+//                    ErProcessor registeredProcessor = registeredSessionMeta.getProcessors().get(i);
+//                    erProcessorErServerNodeMap.forEach((processor, node) -> {
+//                        processor.setId(registeredProcessor.getId());
+//                    });
+//                    pariList.add(new Pair<>(erProcessorErServerNodeMap, registeredProcessor));
+//                }
+//
+//                List<Pair<Long, ErServerNode>> deepspeedConfigsWithNode = new ArrayList<>();
+//                List<DeepspeedContainerConfig> deepspeedContainerConfigList = new ArrayList<>();
+//                AtomicInteger globalRank = new AtomicInteger(0);
+//                AtomicInteger crossSize = new AtomicInteger(0);
+//                AtomicInteger crossRank = new AtomicInteger(0);
+//                Map<ErProcessor, List<Pair<Map<ErProcessor, ErServerNode>, ErProcessor>>> collect = pariList.stream()
+//                        .collect(Collectors.groupingBy(Pair::getValue));
+//                collect.forEach((node,processorAndNodeArray)->{
+//                    crossSize.getAndIncrement();
+//                    int localSize = processorAndNodeArray.size();
+//                    List<Integer> cudaVisibleDevices = new ArrayList<>();
+//                    processorAndNodeArray.forEach(pair -> {
+//                        String[] devicesForProcessor = pair.getValue().getOptions().getOrDefault("cudaVisibleDevices", "-1").split(",");
+//                        for (String devicesStr : devicesForProcessor) {
+//                            int device = Integer.parseInt(devicesStr);
+//                            cudaVisibleDevices.add(device);
+//                            if (device < 0) {
+//                                throw new IllegalArgumentException("cudaVisibleDevices is not set or invalid: " + JsonUtil.object2Json(pair.getKey().getOptions().get("cudaVisibleDevices")));
+//                            }
+//                        }
+//                    });
+//
+//                    if (cudaVisibleDevices.stream().distinct().count() != cudaVisibleDevices.size()) {
+//                        throw new IllegalArgumentException("duplicate cudaVisibleDevices: " + JsonUtil.object2Json(cudaVisibleDevices));
+//                    }
+//
+//                    int localRank = 0;
+//                    for (Pair<Map<ErProcessor, ErServerNode>, ErProcessor> pair : processorAndNodeArray) {
+//                        DeepspeedContainerConfig deepspeedContainerConfig = new DeepspeedContainerConfig();
+//                        deepspeedContainerConfig.setCudaVisibleDevices(cudaVisibleDevices);
+//                        deepspeedContainerConfig.setWorldSize(worldSize);
+//                        deepspeedContainerConfig.setCrossRank(crossRank.get());
+//                        deepspeedContainerConfig.setCrossSize(crossSize.get());
+//                        deepspeedContainerConfig.setLocalSize(localSize);
+//                        deepspeedContainerConfig.setLocalRank(localRank);
+//                        deepspeedContainerConfig.setRank(globalRank.get());
+//                        deepspeedContainerConfig.setStorePrefix(sessionId);
+//                        deepspeedConfigsWithNode.add(new Pair<Long, ErServerNode>(
+//                                pair.getValue().getId(),
+//                                node));
+//                        deepspeedContainerConfigList.add(deepspeedContainerConfig);
+//                        localRank++;
+//                        globalRank.addAndGet(1);
+//                    }
+//
+//                    crossRank.addAndGet(1);
+//                });
+//                long[] ranksProcessorId = deepspeedConfigsWithNode.stream().mapToLong(Triple::getFirst).toArray();
+//                long[] ranksNodeId = deepspeedConfigsWithNode.stream().mapToLong(triple -> triple.getSecond().getId()).toArray();
+//                int[] ranksLocalRank = deepspeedConfigsWithNode.stream().mapToInt(Triple::getThird).toArray();
+//                int[] ranksRank = deepspeedConfigsWithNode.stream().mapToInt(triple -> triple.getThird().getRank()).toArray();
+//
+//                smDao.registerRanks(sessionId, ranksProcessorId, ranksNodeId, ranksLocalRank, ranksRank);
+//
+//                deepspeedConfigsWithNode.parallelStream().forEach(nodeAndConfigs -> {
+//                    ErServerNode node = nodeAndConfigs.getSecond();
+//                    Map<Long, DeepspeedContainerConfig> deepspeedConfigs = nodeAndConfigs.getThird().stream()
+//                            .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond));
+//
+//                    NodeManagerClient nodeManagerClient = new NodeManagerClient(node.getEndpoint());
+//                    nodeManagerClient.startJobContainers(new StartDeepspeedContainerRequest(
+//                            sessionId,
+//                            submitJobRequest.getName(),
+//                            submitJobRequest.getCommandArguments(),
+//                            submitJobRequest.getEnvironmentVariables(),
+//                            submitJobRequest.getFiles(),
+//                            submitJobRequest.getZippedFiles(),
+//                            deepspeedConfigs,
+//                            submitJobRequest.getOptions()
+//                    ));
+//                });
+//
+//                long startTimeout = System.currentTimeMillis() + SessionConfKeys.EGGROLL_SESSION_START_TIMEOUT_MS.get();
+//                ErProcessor[] activeProcessors = waitSubmittedContainers(sessionId, worldSize, startTimeout);
+//
+//                Map<Long, Map<String, String>> idToOptions = Arrays.stream(dispatchedProcessors)
+//                        .collect(Collectors.toMap(ErProcessor::getId, ErProcessor::getOptions));
+//
+//                for (ErProcessor processor : activeProcessors) {
+//                    Map<String, String> options = idToOptions.get(processor.getId());
+//                    processor.getOptions().putAll(options);
+//                }
+//
+//                smDao.updateSessionStatus(sessionId, SessionStatus.ACTIVE, SessionStatus.NEW);
+//
+//                return new SubmitJobResponse(sessionId, Arrays.asList(activeProcessors));
+//            } else {
+//                logError("kill session " + sessionId + " request was found");
+//                throw new ErSessionException("kill session " + sessionId + " request was found");
+//            }
+//        } catch (Exception e) {
+//            killJob(sessionId, false);
+//            throw e;
+//        } finally {
+//            ClusterResourceManager.unlockSession(sessionId);
+//        }
 //    }
 }
