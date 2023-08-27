ccfeddddbc1c2198b59c62d4a419bf1fb9b0408f
forgive_dengkai <forgive_dengkai@163.com>
2023-08-27 09:40:44 +0800

    develop
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/GuiceTest.java
+++ jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/GuiceTest.java
@@ -4,9 +4,11 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.webank.eggroll.clustermanager.dao.mapper.ServerNodeMapper;
 import com.webank.eggroll.guice.module.ClusterModule;
-
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 public class GuiceTest {
 
+    static  Logger  logger = LoggerFactory.getLogger(GuiceTest.class);
 
     public static  void main(String[] args){
 
@@ -16,6 +18,6 @@ public class GuiceTest {
         // Bootstrap the application by creating an instance of the server then
         // start the server to handle incoming requests.
       ServerNodeMapper  nodeMapper =  injector.getInstance(ServerNodeMapper.class);
-      System.err.println(nodeMapper.selectById(1));
+        logger.info("{}",nodeMapper.selectById(1));
     }
 }
