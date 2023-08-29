899f5ea58cc4650c2405c2fc23f60f3a7e460e16
forgive_dengkai <forgive_dengkai@163.com>
2023-08-29 09:02:26 +0800

    develop
    Signed-off-by: forgive_dengkai <forgive_dengkai@163.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/guice/module/MybatisPlusConfigurationProvider.java
+++ jvm/cluster_manager/src/main/java/com/webank/eggroll/guice/module/MybatisPlusConfigurationProvider.java
@@ -176,7 +176,7 @@ public class MybatisPlusConfigurationProvider implements Provider<Configuration>
         configuration.setObjectWrapperFactory(new MybatisMapWrapperFactory());
 
 
-        configuration.setLogImpl(Log4j2Impl.class);
+        configuration.setLogImpl(StdOutImpl.class);
 
         Set<Class<?>> classes  =getClasses(MetaInfo.EGGROLL_MYBATIS_MAPPER_PACKAGE);
 
