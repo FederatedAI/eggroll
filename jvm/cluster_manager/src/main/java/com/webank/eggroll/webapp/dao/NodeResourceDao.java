11b4f26cb57d7b24780583d7ce90deb9d8506cfc
v_ndu <598671894@qq.com>
2023-09-14 09:59:39 +0800

    fix pageHelper
    Signed-off-by: v_ndu <598671894@qq.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/webapp/dao/NodeResourceDao.java
+++ jvm/cluster_manager/src/main/java/com/webank/eggroll/webapp/dao/NodeResourceDao.java
@@ -2,10 +2,13 @@ package com.webank.eggroll.webapp.dao;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.webank.eggroll.clustermanager.dao.impl.NodeResourceService;
 import com.webank.eggroll.clustermanager.entity.NodeResource;
+import com.webank.eggroll.clustermanager.entity.SessionProcessor;
 
 import java.util.List;
 
@@ -15,9 +18,7 @@ public class NodeResourceDao {
     @Inject
     NodeResourceService nodeResourceService;
     public List<NodeResource> getData(int page, int pageSize) {
-        IPage<NodeResource> pageStats = new Page<>();
-        pageStats.setSize(pageSize);
-        pageStats.setCurrent(page);
-        return this.nodeResourceService.list(pageStats);
+        PageHelper.startPage(page, pageSize);
+        return this.nodeResourceService.list();
     }
 }
