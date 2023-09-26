8dfac37a4428fbc480987ecc476267abe5db89b7
v_wbxiongli <740332065@qq.com>
2023-09-26 15:52:38 +0800

    fix start process
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/new_core/src/main/java/com/eggroll/core/containers/container/PythonContainerRuntimeConfig.java
+++ jvm/new_core/src/main/java/com/eggroll/core/containers/container/PythonContainerRuntimeConfig.java
@@ -2,6 +2,7 @@ package com.eggroll.core.containers.container;
 
 import com.eggroll.core.config.ErConf;
 import com.eggroll.core.pojo.StaticErConf;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.*;
 import java.util.function.Supplier;
@@ -36,27 +37,38 @@ public class PythonContainerRuntimeConfig extends ErConf {
     }
 
     public String getPythonExec(String key) {
-        List<Supplier<Optional<String>>> fallbacks = new ArrayList<>();
-
-        fallbacks.add(() -> Optional.ofNullable(key)
-                .filter(s -> !s.isEmpty())
-                .map(k -> this.getString(k,""))
-        );
-
-        fallbacks.add(() -> Optional.ofNullable(this.getString(Container.ContainerKey.PYTHON_EXEC,"")));
-
-        fallbacks.add(() -> Optional.ofNullable(this.getString(Container.ContainerKey.EGGPAIR_VENV,""))
-                .filter(s -> !s.isEmpty())
-                .map(v -> v + "/bin/python")
-        );
-
-        for (Supplier<Optional<String>> f : fallbacks) {
-            Optional<String> value = f.get();
-            if (value.isPresent()) {
-                return value.get();
-            }
+//        List<Supplier<Optional<String>>> fallbacks = new ArrayList<>();
+//
+//        fallbacks.add(() -> Optional.ofNullable(key)
+//                .filter(s -> !s.isEmpty())
+//                .map(k -> this.getString(k,""))
+//        );
+//
+//        fallbacks.add(() -> Optional.ofNullable(this.getString(Container.ContainerKey.PYTHON_EXEC,"")));
+//
+//        fallbacks.add(() -> Optional.ofNullable(this.getString(Container.ContainerKey.EGGPAIR_VENV,""))
+//                .filter(s -> !s.isEmpty())
+//                .map(v -> v + "/bin/python")
+//        );
+//
+//        for (Supplier<Optional<String>> f : fallbacks) {
+//            Optional<String> value = f.get();
+//            if (value.isPresent()) {
+//                return value.get();
+//            }
+//        }
+        String value = getString(key, null);
+        if (StringUtils.isNotBlank(value)) {
+            return value;
         }
-
+        value = getString(Container.ContainerKey.PYTHON_EXEC, null);
+        if (StringUtils.isNotBlank(value)) {
+            return value;
+        }
+        value = getString(Container.ContainerKey.EGGPAIR_VENV, null);
+        if (StringUtils.isNotBlank(value)) {
+            return value + "/bin/python";
+        }
         throw new RuntimeException("python exec not found for key: " + key);
 //        for (String fallback : fallbacks) {
 //            String value = fallback;
