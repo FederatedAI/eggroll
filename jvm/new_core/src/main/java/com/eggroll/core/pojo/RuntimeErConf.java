46935ce33f0a0212b0d5b82b442de4be2a399d34
v_wbxiongli <740332065@qq.com>
2023-09-26 16:33:28 +0800

    fix conf
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/new_core/src/main/java/com/eggroll/core/pojo/RuntimeErConf.java
+++ jvm/new_core/src/main/java/com/eggroll/core/pojo/RuntimeErConf.java
@@ -1,47 +1,47 @@
-package com.eggroll.core.pojo;
-
-import com.eggroll.core.config.Dict;
-import com.eggroll.core.config.ErConf;
-import com.eggroll.core.config.MetaInfo;
-
-import java.util.Map;
-import java.util.Properties;
-
-@SuppressWarnings("unused")
-public class RuntimeErConf extends ErConf {
-    public RuntimeErConf(Properties prop) {
-        super();
-        ErConf.getConf().putAll(prop);
-    }
-
-    public RuntimeErConf(ErSessionMeta sessionMeta) {
-        super();
-        for (Map.Entry<String, String> entry : sessionMeta.getOptions().entrySet()) {
-            ErConf.getConf().put(entry.getKey(), entry.getValue());
-        }
-        ErConf.getConf().put(Dict.CONFKEY_SESSION_ID, sessionMeta.getId());
-        ErConf.getConf().put(Dict.CONFKEY_SESSION_NAME, sessionMeta.getName());
-    }
-
-    public RuntimeErConf(StartContainersRequest startContainersRequest) {
-        super();
-        for (Map.Entry<String, String> entry : startContainersRequest.options.entrySet()) {
-            ErConf.getConf().put(entry.getKey(), entry.getValue());
-        }
-        ErConf.getConf().put(Dict.CONFKEY_SESSION_ID, startContainersRequest.sessionId);
-        ErConf.getConf().put(Dict.CONFKEY_SESSION_NAME, startContainersRequest.name);
-    }
-
-    public RuntimeErConf(Map<String, String> conf) {
-        super();
-        ErConf.getConf().putAll(conf);
-    }
-
-//    public static int getPort() {
-//        return StaticErConf.getPort();
+//package com.eggroll.core.pojo;
+//
+//import com.eggroll.core.config.Dict;
+//import com.eggroll.core.config.ErConf;
+//import com.eggroll.core.config.MetaInfo;
+//
+//import java.util.Map;
+//import java.util.Properties;
+//
+//@SuppressWarnings("unused")
+//public class RuntimeErConf extends ErConf {
+//    public RuntimeErConf(Properties prop) {
+//        super();
+//        ErConf.getConf().putAll(prop);
 //    }
 //
-//    public static String getModuleName() {
-//        return StaticErConf.getModuleName();
+//    public RuntimeErConf(ErSessionMeta sessionMeta) {
+//        super();
+//        for (Map.Entry<String, String> entry : sessionMeta.getOptions().entrySet()) {
+//            ErConf.getConf().put(entry.getKey(), entry.getValue());
+//        }
+//        ErConf.getConf().put(Dict.CONFKEY_SESSION_ID, sessionMeta.getId());
+//        ErConf.getConf().put(Dict.CONFKEY_SESSION_NAME, sessionMeta.getName());
 //    }
-}
+//
+//    public RuntimeErConf(StartContainersRequest startContainersRequest) {
+//        super();
+//        for (Map.Entry<String, String> entry : startContainersRequest.options.entrySet()) {
+//            ErConf.getConf().put(entry.getKey(), entry.getValue());
+//        }
+//        ErConf.getConf().put(Dict.CONFKEY_SESSION_ID, startContainersRequest.sessionId);
+//        ErConf.getConf().put(Dict.CONFKEY_SESSION_NAME, startContainersRequest.name);
+//    }
+//
+//    public RuntimeErConf(Map<String, String> conf) {
+//        super();
+//        ErConf.getConf().putAll(conf);
+//    }
+//
+////    public static int getPort() {
+////        return StaticErConf.getPort();
+////    }
+////
+////    public static String getModuleName() {
+////        return StaticErConf.getModuleName();
+////    }
+//}
