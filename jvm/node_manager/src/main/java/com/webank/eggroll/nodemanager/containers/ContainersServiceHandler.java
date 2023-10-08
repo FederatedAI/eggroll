3205632bb557cb83b8c965ff49c18e21a11a1b8a
v_wbxiongli <740332065@qq.com>
2023-10-08 16:21:20 +0800

    fix log
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/node_manager/src/main/java/com/webank/eggroll/nodemanager/containers/ContainersServiceHandler.java
+++ jvm/node_manager/src/main/java/com/webank/eggroll/nodemanager/containers/ContainersServiceHandler.java
@@ -236,7 +236,7 @@ public class ContainersServiceHandler {
 
         // 获取日志文件路径
         Path path = getContainerLogsDir(sessionId, rank);
-        path = path.resolve(StringUtils.isNotBlank(request.getLogType()) ? request.getLogType() : "INFO").resolve(".log");
+        path = path.resolve((StringUtils.isNotBlank(request.getLogType()) ? request.getLogType() : "INFO") + ".log");
 
         if (!path.toFile().exists()) {
             throw new PathNotExistException("Can not find file " + path);
