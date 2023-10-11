f20b18492579ef313a9a39991201816f8151c21a
v_wbxiongli <740332065@qq.com>
2023-10-11 18:07:06 +0800

    fix
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/cluster/ClusterResourceManager.java
+++ jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/cluster/ClusterResourceManager.java
@@ -41,7 +41,7 @@ public class ClusterResourceManager implements ApplicationStartedRunner {
 
     Logger log = LoggerFactory.getLogger(ClusterResourceManager.class);
 
-    public static ConcurrentHashMap<String, ReentrantLock> sessionLockMap = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, ReentrantLock> sessionLockMap = new ConcurrentHashMap<>();
     private Map<String, Long> killJobMap = new ConcurrentHashMap<>();
     private FifoBroker<ResourceApplication> applicationQueue = new FifoBroker<>();
     @Inject
