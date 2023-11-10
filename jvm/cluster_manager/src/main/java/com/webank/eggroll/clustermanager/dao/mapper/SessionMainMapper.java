b33332b636ae083ccf7fe25ee0c23181f4f91e58
v_wbxiongli <740332065@qq.com>
2023-11-10 10:02:57 +0800

    add mybatis cache
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/dao/mapper/SessionMainMapper.java
+++ jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/dao/mapper/SessionMainMapper.java
@@ -2,6 +2,8 @@ package com.webank.eggroll.clustermanager.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.webank.eggroll.clustermanager.entity.SessionMain;
+import org.apache.ibatis.annotations.CacheNamespace;
 
+@CacheNamespace(flushInterval = 60000, size = 128, readWrite = false)
 public interface SessionMainMapper extends BaseMapper<SessionMain> {
 }
