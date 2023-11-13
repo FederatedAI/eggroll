d05ac7e8f53de18ad97a113242e53f4729528a02
wmqwxb <13628607970@163.com>
2023-11-13 15:58:32 +0800

    expire time from metainfo
    Signed-off-by: wmqwxb <13628607970@163.com>

--- jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/cluster/ClusterManagerService.java
+++ jvm/cluster_manager/src/main/java/com/webank/eggroll/clustermanager/cluster/ClusterManagerService.java
@@ -68,7 +68,7 @@ public class ClusterManagerService implements ApplicationStartedRunner {
     public static Map<Long, ErProcessor> residualHeartbeatMap = new ConcurrentHashMap<>();
     Cache<Long, ReentrantLock> heartbeatLockCache = CacheBuilder.newBuilder()
             .maximumSize(100)
-            .expireAfterAccess(MetaInfo.CONFKEY_NODE_MANAGER_HEARTBEAT_INTERVAL * 5, TimeUnit.SECONDS)
+            .expireAfterAccess(MetaInfo.CONFKEY_NODE_MANAGER_HEARTBEAT_INTERVAL * 10, TimeUnit.MILLISECONDS)
             .build();
 
     public void addResidualHeartbeat(ErProcessor erProcessor) {
