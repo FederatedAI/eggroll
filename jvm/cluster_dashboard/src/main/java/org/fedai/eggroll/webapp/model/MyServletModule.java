1e08fd1e1fbbaab948c71e618429eb3bb2a2a5dd
v_ndu <598671894@qq.com>
2023-11-16 14:55:49 +0800

    fix
    Signed-off-by: v_ndu <598671894@qq.com>

--- jvm/cluster_dashboard/src/main/java/org/fedai/eggroll/webapp/model/MyServletModule.java
+++ jvm/cluster_dashboard/src/main/java/org/fedai/eggroll/webapp/model/MyServletModule.java
@@ -24,6 +24,7 @@ public class MyServletModule extends ServletModule {
         bind(DispatcherServlet.class).in(Singleton.class);
         bind(EggrollServiceProvider.class).in(Singleton.class);
         bind(UserController.class).in(Singleton.class);
+
         bind(SecurityService.class).to(RSASecurityService.class);
 
         // 绑定ZookeeperQueryService,并从配置文件读取zk服务器地址，创建连接实例（获取zk服务器信息接口）
