a1e86f5e79ff69b34d1c3a43920a47c8080278ab
v_wbxiongli <740332065@qq.com>
2023-11-17 17:19:32 +0800

    update session status
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- jvm/cluster_manager/src/main/java/org/fedai/eggroll/clustermanager/statemachine/AbstractSessionStateHandler.java
+++ jvm/cluster_manager/src/main/java/org/fedai/eggroll/clustermanager/statemachine/AbstractSessionStateHandler.java
@@ -28,11 +28,13 @@ public abstract class AbstractSessionStateHandler implements StateHandler<ErSess
     SessionOptionService sessionOptionService;
 
     void updateStatus(Context context, ErSessionMeta erSessionMeta, String preStateParam, String desStateParam) {
+        final ErSessionMeta dbSession = sessionMainService.getSession(erSessionMeta.getId());
         SessionMain sessionMain = new SessionMain();
         sessionMain.setSessionId(erSessionMeta.getId());
         sessionMain.setStatus(desStateParam);
         sessionMain.setActiveProcCount(erSessionMeta.getActiveProcCount());
         sessionMain.setBeforeStatus(preStateParam);
+        sessionMain.setBeforeStatus(dbSession.getStatus());
         sessionMainService.updateById(sessionMain);
     }
 
