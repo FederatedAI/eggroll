a44844b9a2c298bf2a8218bf00b75d1938b345bd
wmqwxb <13628607970@163.com>
2023-11-23 18:51:07 +0800

    add api for query cluster info
    Signed-off-by: wmqwxb <13628607970@163.com>

---
+++ jvm/core/src/main/java/org/fedai/eggroll/core/pojo/ServerCluster.java
@@ -0,0 +1,56 @@
+package org.fedai.eggroll.core.pojo;
+
+import com.webank.eggroll.core.meta.Containers;
+import com.webank.eggroll.core.meta.Meta;
+import lombok.Data;
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class ServerCluster implements RpcMessage {
+
+    private List<ErServerNode> serverNodes;
+
+
+    public Meta.ServerCluster toProto() {
+        Meta.ServerCluster.Builder builder = Meta.ServerCluster.newBuilder();
+        List<Meta.ServerNode> list = new ArrayList<>();
+        serverNodes.forEach(erServerNode -> {
+            list.add(erServerNode.toProto());
+        });
+        builder.addAllServerNodes(list);
+        return builder.build();
+    }
+
+    public ServerCluster fromProto(Meta.ServerCluster serverCluster) {
+        ServerCluster cluster = new ServerCluster();
+        cluster.deserialize(serverCluster.toByteArray());
+        return cluster;
+    }
+
+    @Override
+    public byte[] serialize() {
+        return toProto().toByteArray();
+    }
+
+
+    @Override
+    public void deserialize(byte[] data) {
+        try {
+            Meta.ServerCluster response = Meta.ServerCluster.parseFrom(data);
+            List<Meta.ServerNode> serverNodesList = response.getServerNodesList();
+            if (CollectionUtils.isNotEmpty(serverNodesList)) {
+                List<ErServerNode> serverNodeList = new ArrayList<>();
+                serverNodesList.forEach(serverNode -> {
+                    serverNodeList.add(ErServerNode.fromProto(serverNode));
+                });
+                this.serverNodes = serverNodeList;
+            }
+        } catch (Exception e) {
+
+        }
+
+    }
+}
