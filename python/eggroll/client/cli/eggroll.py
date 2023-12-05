5b665402a7a861730523cb9d3c43ee140fe59e71
sagewe <wbwmat@gmail.com>
2023-12-05 19:03:54 +0800

    fix: eggroll client

--- python/eggroll/client/cli/eggroll.py
+++ python/eggroll/client/cli/eggroll.py
@@ -19,88 +19,124 @@ import click
 from ruamel import yaml
 
 from eggroll.client.cli.commands import task
-from eggroll.client.cli.utils.cli_utils import prettify
 from eggroll.client.sdk import EggrollClient
+from eggroll.config import Config
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
-@click.group(short_help='Eggroll Client', context_settings=CONTEXT_SETTINGS)
+@click.group(short_help="Eggroll Client", context_settings=CONTEXT_SETTINGS)
+@click.option("--eggroll-properties", type=click.Path(exists=True), help="eggroll properties file")
 @click.pass_context
-def eggroll_cli(ctx):
-    '''
+def eggroll_cli(ctx, eggroll_properties):
+    """
     Eggroll Client
-    '''
+    """
     ctx.ensure_object(dict)
-    if ctx.invoked_subcommand == 'init':
+    if ctx.invoked_subcommand == "init":
         return
 
-    with open(os.path.join(os.path.dirname(__file__), 'settings.yaml'), 'r') as fin:
-        config = yaml.safe_load(fin)
+    with open(os.path.join(os.path.dirname(__file__), "settings.yaml"), "r") as fin:
+        cli_config = yaml.safe_load(fin)
 
-    if config.get('ip') and config.get('port'):
-        ctx.obj['ip'] = config['ip']
-        ctx.obj['port'] = int(config['port'])
+    if cli_config.get("ip") and cli_config.get("port"):
+        ctx.obj["ip"] = cli_config["ip"]
+        ctx.obj["port"] = int(cli_config["port"])
     else:
         raise ValueError('Invalid configuration file. Did you run "flow init"?')
 
-    ctx.obj['initialized'] = (config.get('ip') and config.get('port'))
-    if ctx.obj['initialized']:
-        ctx.obj["client"] = EggrollClient(
-            ip=config.get('ip'), port=config.get('port')
-        )
+    ctx.obj["initialized"] = cli_config.get("ip") and cli_config.get("port")
+    if ctx.obj["initialized"]:
+        config = Config()
+        config.load_default()
+        if eggroll_properties:
+            config.load_properties(eggroll_properties)
+        config.load_env()
+        ctx.obj["client"] = EggrollClient(config=config, ip=cli_config.get("ip"), port=cli_config.get("port"))
 
 
-@eggroll_cli.command('init', short_help='Eggroll CLI Init Command')
-@click.option('--ip', type=click.STRING, help='Eggroll gip address.')
-@click.option('--port', type=click.INT, help='Eggroll grpc port.')
-def initialization(**kwargs):
-    with open(os.path.join(os.path.dirname(__file__), 'settings.yaml'), 'r') as fin:
-        config = yaml.safe_load(fin)
+@eggroll_cli.command("init", short_help="Eggroll CLI Init Command")
+@click.option("--ip", type=click.STRING, help="Eggroll gip address.")
+@click.option("--port", type=click.INT, help="Eggroll grpc port.")
+@click.pass_context
+def initialization(ctx: click.Context, ip, port, **kwargs):
+    if (ip is None) and (port is None):
+        click.secho("[Error]: Please specify ip or port.\n", fg="red")
+        click.echo(ctx.get_help())
+        ctx.exit(100)
 
-    if kwargs.get('reset'):
-        for i in ('ip', 'port', 'app_id', 'app_token'):
-            config[i] = None
-
-        with open(os.path.join(os.path.dirname(__file__), 'settings.yaml'), 'w') as fout:
-            yaml.dump(config, fout, Dumper=yaml.RoundTripDumper)
-        prettify(
-            {
-                'retcode': 0,
-                'retmsg': 'Eggroll CLI has been reset successfully'
-            }
+    config = {}
+    if not os.path.exists(path := _get_config_path()):
+        click.secho(
+            f"[Warn ]: config file not found in {path}, will create one.", fg="yellow"
         )
     else:
-        _update = False
-        if not config:
-            config = {}
-        for i in ('ip', 'port'):
-            if kwargs.get(i):
-                config[i] = kwargs[i]
-                _update = True
+        with open(_get_config_path(), "r") as fin:
+            config = yaml.safe_load(fin)
 
-        if _update:
-            with open(os.path.join(os.path.dirname(__file__), 'settings.yaml'), 'w') as fout:
-                # yaml.dump(config, fout, Dumper=yaml.RoundTripDumper)
-                yaml.dump(config, fout)
-            prettify(
-                {
-                    'retcode': 0,
-                    'retmsg': 'Eggroll CLI has been initialized successfully.'
-                }
+    _update = False
+
+    def _update_config(_config, _key, _value):
+        if (old_value := _config.get(_key)) != _value:
+            _config[_key] = _value
+            click.secho(
+                f"[Info ]update {_key} from {old_value} to {_value}", fg="green"
             )
+            nonlocal _update
+            _update = True
+
+    if ip is not None:
+        _update_config(config, "ip", ip)
+    if port is not None:
+        _update_config(config, "port", port)
+
+    def _validate_ip(_config):
+        if (_ip := _config.get("ip")) is None:
+            click.secho(f"[Error]ip not set", fg="red")
+            ctx.exit(100)
         else:
-            prettify(
-                {
-                    'retcode': 100,
-                    'retmsg': 'Eggroll CLI initialization failed.'
-                }
-            )
+            try:
+                import ipaddress
+
+                ipaddress.IPv4Address(_ip)
+                click.secho(f"[Info ]ip format {_ip} is valid", fg="green")
+            except Exception:
+                click.secho(f"[Error]ip format: {_ip} is invalid", fg="red")
+                ctx.exit(100)
+
+    _validate_ip(config)
+
+    def _validate_port(_config):
+        if (_port := _config.get("port")) is None:
+            click.secho(f"[Error]port not set", fg="red")
+            ctx.exit(100)
+        else:
+            try:
+                _port = int(_port)
+                if not (0 < _port < 65535):
+                    raise ValueError
+                click.secho(f"[Info ]port format: {_port} is valid", fg="green")
+            except Exception:
+                click.secho(f"[Error]port format: {_port} is invalid", fg="red")
+                ctx.exit(100)
+
+    _validate_port(config)
+
+    if _update:
+        with open(_get_config_path(), "w") as fout:
+            yaml.dump(config, fout, Dumper=yaml.RoundTripDumper)
+        click.secho(f"[Info ]config file has been successfully updated", fg="green")
+    else:
+        click.secho(f"[Info ]config file has not been changed", fg="green")
+
+
+def _get_config_path():
+    return os.path.join(os.path.dirname(__file__), "settings.yaml")
 
 
 eggroll_cli.add_command(task.task)
 
 
-if __name__ == '__main__':
-    print(f'--start--')
+if __name__ == "__main__":
+    print(f"--start--")
     eggroll_cli()
