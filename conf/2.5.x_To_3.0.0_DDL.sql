ae3c0afe1dc6b9cf60df0a9adfc01d10324eaeec
v_ndu <598671894@qq.com>
2023-12-06 16:21:57 +0800

    fix doc
    Signed-off-by: v_ndu <598671894@qq.com>

--- conf/2.5.x_To_3.0.0_DDL.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `session_main`
-	ADD COLUMN `status_reason` VARCHAR(255) NULL AFTER `status`,
-	ADD COLUMN `before_status` VARCHAR(255) NULL AFTER `status_reason`;
-ALTER TABLE `session_processor`
-	ADD COLUMN `before_status` VARCHAR(255) NULL AFTER `status_reason`;
