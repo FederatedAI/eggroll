9a4d415c15215308dfe00d8d83ac51a6a15b0dbc
v_wbxiongli <740332065@qq.com>
2023-12-08 15:05:06 +0800

    rm python venv
    Signed-off-by: v_wbxiongli <740332065@qq.com>

--- venv/Lib/site-packages/lmdb/_config.py
+++
@@ -1,2 +0,0 @@
-CONFIG = dict((('extra_compile_args', ['-DHAVE_PATCHED_LMDB=1', '-UNDEBUG', '-w', '/FIPython.h']), ('extra_sources', ['build/lib/mdb.c', 'build/lib/midl.c']), ('extra_library_dirs', []), ('extra_include_dirs', ['lib/py-lmdb', 'build/lib', 'lib\\win32']), ('libraries', ['Advapi32'])))
-
