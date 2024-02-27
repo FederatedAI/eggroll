5d50be9e2cf25f6bf58add31deecf3493cca50d2
sagewe <wbwmat@gmail.com>
2024-02-27 17:42:26 +0800

    feat: add whitelist serde (#708)
    Signed-off-by: sagewe <wbwmat@gmail.com>

--- python/eggroll/backport/_serdes.py
+++ python/eggroll/backport/_serdes.py
@@ -14,18 +14,83 @@
 #
 #
 
-from pickle import dumps as p_dumps
-from pickle import loads as p_loads
+import importlib
+from pickle import Unpickler, UnpicklingError
+# from pickle import dumps as p_dumps
+# from pickle import loads as p_loads
 
 
-class UnrestrictedSerdes:
-    @staticmethod
-    def serialize(obj) -> bytes:
-        return p_dumps(obj)
+# class UnrestrictedSerdes:
+#     @staticmethod
+#     def serialize(obj) -> bytes:
+#         return p_dumps(obj)
+#
+#     @staticmethod
+#     def deserialize(bytes) -> object:
+#         return p_loads(bytes)
 
-    @staticmethod
-    def deserialize(bytes) -> object:
-        return p_loads(bytes)
+
+class RestrictedUnpickler(Unpickler):
+
+    def _load(self, module, name):
+        try:
+            return super().find_class(module, name)
+        except:
+            return getattr(importlib.import_module(module), name)
+
+    def find_class(self, module, name):
+        if name in _DeserializeWhitelist.get_whitelist().get(module, set()):
+            return self._load(module, name)
+        else:
+            for m in _DeserializeWhitelist.get_whitelist_glob():
+                if module.startswith(m):
+                    return self._load(module, name)
+        raise UnpicklingError(f"forbidden unpickle class {module} {name}")
 
 
-Serdes = UnrestrictedSerdes
+class _DeserializeWhitelist:
+    loaded = False
+    deserialize_whitelist = {}
+    deserialize_glob_whitelist = set()
+
+    @classmethod
+    def get_whitelist_glob(cls):
+        if not cls.loaded:
+            cls.load_deserialize_whitelist()
+        return cls.deserialize_glob_whitelist
+
+    @classmethod
+    def get_whitelist(cls):
+        if not cls.loaded:
+            cls.load_deserialize_whitelist()
+        return cls.deserialize_whitelist
+
+    @classmethod
+    def get_whitelist_path(cls):
+        import os.path
+
+        return os.path.abspath(
+            os.path.join(
+                __file__,
+                os.path.pardir,
+                os.path.pardir,
+                os.path.pardir,
+                os.path.pardir,
+                "conf",
+                "whitelist.json",
+            )
+        )
+
+    @classmethod
+    def load_deserialize_whitelist(cls):
+        import json
+        with open(cls.get_whitelist_path()) as f:
+            for k, v in json.load(f).items():
+                if k.endswith("*"):
+                    cls.deserialize_glob_whitelist.add(k[:-1])
+                else:
+                    cls.deserialize_whitelist[k] = set(v)
+        cls.loaded = True
+
+
+Serdes = RestrictedUnpickler
